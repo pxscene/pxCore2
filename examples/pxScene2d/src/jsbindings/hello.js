0ff81e249711f21f5b6faf6bd61ba3b7d65abd18
jacobgladish <jacobgladish@yahoo.com>
2015-01-25 19:55:16 -0500

    Adding pxOffscreen support.

--- examples/pxScene2d/src/jsbindings/hello.js
+++ examples/pxScene2d/src/jsbindings/hello.js
@@ -1,13 +1,14 @@
 var px = require('./build/Release/px');
 var eventLoop = new px.EventLoop();
 var win = new px.Window(50, 50, 6480, 480);
+win.texture = new px.Offscreen();
 win.title = 'Hello World!';
 win.on('closerequest', function() { eventLoop.exit(); });
 win.on('keydown', function(code, flags) {
   console.log('code:' + code);
 });
 win.on('resize', function(width, height) {
-  consoole.log('w: ' + width + ' h: ' + height);
+  win.texture.init(width, height);
 });
 win.visible = true;
 
