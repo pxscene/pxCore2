02bd51dfff3563517aebcb8843aa71e695d6be86
John Robinson <johnrobinsn@gmail.com>
2015-01-31 08:31:24 -0500

    tweaked initial window size to be 16:9

--- examples/pxScene2d/src/jsbindings/rtWrapperInit.cpp
+++ examples/pxScene2d/src/jsbindings/rtWrapperInit.cpp
@@ -177,7 +177,7 @@ static Handle<Value> getScene(const Arguments& args)
 {
   if (mainWindow == NULL)
   {
-    mainWindow = new jsWindow(0, 0, 640, 480);
+    mainWindow = new jsWindow(0, 0, 1280, 720);
 
     char title[]= { "pxScene from JavasScript!" };
     mainWindow->setTitle(title);
