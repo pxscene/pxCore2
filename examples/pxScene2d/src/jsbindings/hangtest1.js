36f76033eebb1a85c4169a0fa856043218452b3f
John Robinson <johnrobinsn@gmail.com>
2015-02-02 16:51:57 -0500

    Added test to try and verify render thread still renders while js busy looping

---
+++ examples/pxScene2d/src/jsbindings/hangtest1.js
@@ -0,0 +1,28 @@
+var px = require("./build/Debug/px");
+
+var scene = px.getScene();
+var text = scene.createText();
+text.text = "This should be spinning";
+text.x = 50;
+text.rx = 1;
+text.rz = 0;
+text.parent = scene.root;
+text.animateTo("r", 360, 1.0, 0, 2);
+
+scene.on('resize', function(width, height) {
+  console.log('resize:' + width + ' height:' + height);
+    text.y = height/2;
+
+// Make sure the render loop still keeps going
+while(true);
+
+
+});
+
+scene.on('keydown', function(code, flags) {
+  console.log("keydown:" + code);
+});
+
+
+
+
