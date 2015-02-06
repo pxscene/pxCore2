8ae8b8954bec50fa05fb6cabda371634bc39f128
hfitzpatrick <Hugh_Fitzpatrick@cable.comcast.com>
2015-02-06 09:47:30 -0500

    House-keeping

--- examples/Simple/Simple.cpp
+++ examples/Simple/Simple.cpp
@@ -13,62 +13,62 @@ pxEventLoop eventLoop;
 
 void drawBackground(pxBuffer& b)
 {
-    // Fill the buffer with a simple pattern as a function of f(x,y)
-    int w = b.width();
-    int h = b.height();
+  // Fill the buffer with a simple pattern as a function of f(x,y)
+  int w = b.width();
+  int h = b.height();
 
-    for (int y = 0; y < h; y++)
+  for (int y = 0; y < h; y++)
+  {
+    pxPixel* p = b.scanline(y);
+    for (int x = 0; x < w; x++)
     {
-        pxPixel* p = b.scanline(y);
-        for (int x = 0; x < w; x++)
-        {
-            p->r = pxClamp<int>(x+y, 255);
-            p->g = pxClamp<int>(y,   255);
-            p->b = pxClamp<int>(x,   255);
-	    p++;
-        }
+      p->r = pxClamp<int>(x+y, 255);
+      p->g = pxClamp<int>(y,   255);
+      p->b = pxClamp<int>(x,   255);
+      p++;
     }
+  }
 }
 
 class myWindow: public pxWindow
 {
 private:
-    // Event Handlers - Look in pxWindow.h for more
-    void onCloseRequest()
-    {
-        // When someone clicks the close box no policy is predefined.
-        // so we need to explicitly tell the event loop to exit
-	eventLoop.exit();
-    }
+  // Event Handlers - Look in pxWindow.h for more
+  void onCloseRequest()
+  {
+    // When someone clicks the close box no policy is predefined.
+    // so we need to explicitly tell the event loop to exit
+    eventLoop.exit();
+  }
 
-    void onSize(int newWidth, int newHeight)
-    {
-        // When ever the window resizes (re)allocate a buffer big 
-	// enough for the entire client area and draw our pattern into it
-	mTexture.init(newWidth, newHeight);
-	drawBackground(mTexture);
-    }
+  void onSize(int newWidth, int newHeight)
+  {
+    // When ever the window resizes (re)allocate a buffer big
+    // enough for the entire client area and draw our pattern into it
+    mTexture.init(newWidth, newHeight);
+    drawBackground(mTexture);
+  }
 
-    void onDraw(pxSurfaceNative s)
-    {
-        // Draw the texture into this window
-        mTexture.blit(s);
-    }
+  void onDraw(pxSurfaceNative s)
+  {
+    // Draw the texture into this window
+    mTexture.blit(s);
+  }
 
-    pxOffscreen mTexture;
+  pxOffscreen mTexture;
 };
 
 int pxMain()
 {
-    myWindow win;
+  myWindow win;
 
-    win.init(10, 64, 640, 480);
-    win.setTitle("Simple");
-    win.setVisibility(true);
+  win.init(10, 64, 640, 480);
+  win.setTitle("Simple");
+  win.setVisibility(true);
 
-    eventLoop.run();
+  eventLoop.run();
 
-    return 0;
+  return 0;
 }
 
 
--- examples/Sprite/Sprite.cpp
+++ examples/Sprite/Sprite.cpp
@@ -14,81 +14,81 @@ pxEventLoop eventLoop;
 
 void drawBackground(pxBuffer& b)
 {
-    // Fill the buffer with a simple pattern as a function of f(x,y)
-    int w = b.width();
-    int h = b.height();
+  // Fill the buffer with a simple pattern as a function of f(x,y)
+  int w = b.width();
+  int h = b.height();
 
-    for (int y = 0; y < h; y++)
+  for (int y = 0; y < h; y++)
+  {
+    pxPixel* p = b.scanline(y);
+    for (int x = 0; x < w; x++)
     {
-        pxPixel* p = b.scanline(y);
-        for (int x = 0; x < w; x++)
-        {
-            p->r = pxClamp<int>(x+y, 255);
-            p->g = pxClamp<int>(y,   255);
-            p->b = pxClamp<int>(x,   255);
-            p++;
-        }
+      p->r = pxClamp<int>(x+y, 255);
+      p->g = pxClamp<int>(y,   255);
+      p->b = pxClamp<int>(x,   255);
+      p++;
     }
+  }
 }
 
 class myWindow: public pxWindow
 {
 private:
-    // Event Handlers - Look in pxWindow.h for more
-    void onCloseRequest()
-    {
-        // When someone clicks the close box no policy is predefined.
-        // so we need to explicitly tell the event loop to exit
-	    eventLoop.exit();
-    }
+  // Event Handlers - Look in pxWindow.h for more
+  void onCloseRequest()
+  {
+    // When someone clicks the close box no policy is predefined.
+    // so we need to explicitly tell the event loop to exit
+    eventLoop.exit();
+  }
 
-    void onCreate()
-    {
-        x = y = 100;
-        mSprite.initWithColor(32, 32, pxRed);
-        setAnimationFPS(60);
-    }
+  void onCreate()
+  {
+    x = y = 100;
+    mSprite.initWithColor(32, 32, pxRed);
+    setAnimationFPS(60);
+  }
 
-    void onSize(int newWidth, int newHeight)
-    {
-        // When ever the window resizes (re)allocate a buffer big 
-	// enough for the entire client area and draw our pattern into it
-	mTexture.init(newWidth, newHeight);
-	drawBackground(mTexture);
-    }
+  void onSize(int newWidth, int newHeight)
+  {
+    // When ever the window resizes (re)allocate a buffer big
+    // enough for the entire client area and draw our pattern into it
+    mTexture.init(newWidth, newHeight);
+    drawBackground(mTexture);
+  }
 
-    void onDraw(pxSurfaceNative s)
-    {
-        // Draw the texture into this window
-		drawBackground(mTexture);
-        mSprite.blit(mTexture, x, y, 32, 32, 0, 0);
-	    mTexture.blit(s);
-    }
+  void onDraw(pxSurfaceNative s)
+  {
+    // Draw the texture into this window
+    drawBackground(mTexture);
+    mSprite.blit(mTexture, x, y, 32, 32, 0, 0);
+    mTexture.blit(s);
+  }
 
-    void onAnimationTimer()
-    {
-        x = (int)(400*((double)rand()/(double)RAND_MAX));
-        y = (int)(300*((double)rand()/(double)RAND_MAX));
+  void onAnimationTimer()
+  {
+    x = (int)(400*((double)rand()/(double)RAND_MAX));
+    y = (int)(300*((double)rand()/(double)RAND_MAX));
 
-        invalidateRect();
-    }
+    invalidateRect();
+  }
 
-    pxOffscreen mTexture;
-    pxOffscreen mSprite;
-    int x, y;
+  pxOffscreen mTexture;
+  pxOffscreen mSprite;
+  int x, y;
 };
 
 int pxMain()
 {
-    myWindow win;
+  myWindow win;
 
-    win.init(10, 64, 640, 480);
-    win.setTitle("Sprite");
-    win.setVisibility(true);
+  win.init(10, 64, 640, 480);
+  win.setTitle("Sprite");
+  win.setVisibility(true);
 
-    eventLoop.run();
+  eventLoop.run();
 
-    return 0;
+  return 0;
 }
 
 
