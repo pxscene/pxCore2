554a2bf2373d09615d98fcdb2387cf2b5ab6df60
John Robinson <johnrobinsn@gmail.com>
2015-04-17 10:32:34 -0400

    testcase for broken object identity

--- examples/pxScene2d/src/jsbindings/mousetest.js
+++ examples/pxScene2d/src/jsbindings/mousetest.js
@@ -7,9 +7,15 @@ text.cx = text.w/2;
 text.cy = text.h/2;
 //text.animateTo({"r": -360}, 5.0, 0, 2);
 var rTarget = 0;
-text.on("onMouseDown", function() {
+text.on("onMouseDown", function(e) {
     rTarget += 360;
     text.animateTo({r:rTarget}, 1.0, 4, 0); 
+//  console.log(e);
+  if (e.target == text)
+    console.log("object identity working");
+  else
+    console.log("object identiy broken");
+  
 1});
 
 scene.root.id="editorroot";
