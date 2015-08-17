408f6787fb0d8d1bd4d68975e0b7bfcb112734a0
tcarro004 <Thomas_Carroll2@cable.comcast.com>
2015-08-17 13:27:02 -0400

    Addition of rcvrcore, which includes support for px.import.   This commit includes the start.js module used to start an application, as well as an example script that uses start.js.

---
+++ examples/pxScene2d/src/jsbindings/start.js
@@ -0,0 +1,23 @@
+namedContext = "Start Global";
+var setLoggingLevel = require('rcvrcore/Logger').setLoggingLevel;
+
+var argDefinitions = {screenWidth:{required:false, default:1280, help:"Specifies the screen width"},
+  screenHeight:{required:false, default:720, help:"Specifies the screen height"},
+  logLevel:{required:false, default:1, help:"Specifies the logging level (0-N)"},
+  url:{required:true, help:"Specifies JavaScript file to load"}
+};
+
+var argProcessor = require("rcvrcore/utils/ArgProcessor");
+var processArgs = argProcessor(process.argv, argDefinitions);
+
+if( processArgs.hasOwnProperty('logLevel')) {
+  setLoggingLevel(processArgs['logLevel']);
+}
+
+var xreFrameworkBaseUri = processArgs['xreFrameworkBaseUri'];
+console.log("xreFrameworkBaseUri=" + xreFrameworkBaseUri);
+
+// Create root object and send it the base URI of the xre2 framework modules
+var pxRoot = require('rcvrcore/PxRoot')(0, 0, processArgs['screenWidth'], processArgs['screenHeight']);
+
+pxRoot.addScene({url:processArgs['url'],w:processArgs['screenWidth'],h:processArgs['screenHeight']});
