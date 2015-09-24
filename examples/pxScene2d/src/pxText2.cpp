3ed173f6fb1999f5b431d633c4fd345ff212dd46
Nikita Poltorapavlo <npoltorapavlo@productengine.com>
2015-09-24 17:22:33 +0300

    Corrected text measurements, changed descent sign, added a test for text measurements

--- examples/pxScene2d/src/pxText2.cpp
+++ examples/pxScene2d/src/pxText2.cpp
@@ -580,7 +580,7 @@ void pxText2::renderTextRowWithTruncation(rtString & accString, float lineWidth,
 					if( render) {
             mFace->renderText(ELLIPSIS_STR, pixelSize, xPos+charW, tempY, 1.0, 1.0, color,lineWidth);
           } 
-          setMeasurementBounds(xPos+charW, ellipsisW, tempY, charH);
+          setMeasurementBounds(xPos, charW+ellipsisW, tempY, charH);
           setLastLineMeasurements(xPos+charW+ellipsisW, tempY);
 				}
 				break;
@@ -620,7 +620,7 @@ void pxText2::renderTextRowWithTruncation(rtString & accString, float lineWidth,
 					if( render) {
             mFace->renderText(ELLIPSIS_STR, pixelSize, xPos+charW, tempY, 1.0, 1.0, color,lineWidth);
           }
-          setMeasurementBounds(xPos+charW, ellipsisW, tempY, charH);
+          setMeasurementBounds(xPos, charW+ellipsisW, tempY, charH);
           setLastLineMeasurements(xPos+charW+ellipsisW, tempY);
 				}
 				break;
@@ -670,7 +670,7 @@ rtError pxText2::getFontMetrics(rtObjectRef& o) {
 	metrics->setAscent(ascent);
 	metrics->setDescent(descent);
   metrics->setNaturalLeading(naturalLeading);
-  metrics->setBaseline(my+height+descent); // descent is always negative for horizontal layout
+  metrics->setBaseline(my+ascent);
 	o = metrics;
 
 	return RT_OK;
