56cf54ddf0d7b69996a8e0d1ad8daf38bab1af5e
John Robinson <johnrobinsn@gmail.com>
2015-09-25 11:59:18 -0400

    some matrix multiplication improvements... unrolling etc

--- examples/pxScene2d/src/pxMatrix4T.h
+++ examples/pxScene2d/src/pxMatrix4T.h
@@ -83,6 +83,8 @@ public:
     memcpy(mValues, m.mValues, sizeof(mValues));
   }
   
+
+#if 0
   void multiply(pxMatrix4T& mat) {
     FloatT* m = mValues;
     FloatT* n = mat.mValues;
@@ -102,6 +104,49 @@ public:
     }
     memcpy(m, &tmp, sizeof tmp);
   }
+#else
+
+  void multiply(pxMatrix4T& mat) {
+    FloatT* a = mValues;
+    FloatT* b = mat.mValues;
+    
+    FloatT out[16];
+
+    float a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
+        a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7],
+        a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11],
+        a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
+
+    // Cache only the current line of the second matrix
+    float b0  = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
+    out[0] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    out[1] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    out[2] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    out[3] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    b0 = b[4]; b1 = b[5]; b2 = b[6]; b3 = b[7];
+    out[4] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    out[5] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    out[6] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    out[7] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    b0 = b[8]; b1 = b[9]; b2 = b[10]; b3 = b[11];
+    out[8] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    out[9] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    out[10] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    out[11] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    b0 = b[12]; b1 = b[13]; b2 = b[14]; b3 = b[15];
+    out[12] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
+    out[13] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
+    out[14] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
+    out[15] = b0*a03 + b1*a13 + b2*a23 + b3*a33;
+
+    memcpy(a, &out, sizeof out);
+  }
+
+
+#endif
   
   pxVector4f multiply(const pxVector4f& v) {
     pxVector4f out;
@@ -117,6 +162,7 @@ public:
     return out;
   }
     
+#if 0
   void multiply(FloatT* m, FloatT* n) {
     
     FloatT tmp[16];
@@ -134,6 +180,7 @@ public:
     }
     memcpy(m, &tmp, sizeof tmp);
   }
+#endif
   
 #ifdef PX_PLATFORM_GENERIC_EGL
   inline void rotateInRadians(FloatT angle) {
@@ -155,7 +202,8 @@ public:
     rotateInRadians(angle * M_PI/180.0, x, y, z);
   }
   
-  void rotateInRadians(FloatT angle, FloatT x, FloatT y, FloatT z) {
+  void rotateInRadians(FloatT angle, FloatT /*x*/, FloatT /*y*/, FloatT /*z*/) {
+#if 0
     FloatT* m = mValues;
     FloatT s, c;
     
@@ -169,6 +217,9 @@ public:
     };
     
     multiply(m, r);
+#else
+    rotateZInRadians(angle);
+#endif
   }
   
   void rotateZInDegrees(FloatT angle) {
