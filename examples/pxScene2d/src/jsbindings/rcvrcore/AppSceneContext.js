38b52c594f1174132317de8a94be2263b417a79c
John Robinson <johnrobinsn@gmail.com>
2015-11-16 17:40:35 -0500

    Trying to make scene.ready work

--- examples/pxScene2d/src/jsbindings/rcvrcore/AppSceneContext.js
+++ examples/pxScene2d/src/jsbindings/rcvrcore/AppSceneContext.js
@@ -219,6 +219,7 @@ AppSceneContext.prototype.runScriptInNewVMContext = function (code, uri, fromJar
       console.log("Main Module: readyPromise=" + xModule.moduleReadyPromise);
       if( !xModule.hasOwnProperty('moduleReadyPromise') || xModule.moduleReadyPromise === null ) {
         console.log("Main module is ready - no imports: " + this.packageUrl);
+        this.container.makeReady(true);
         this.innerscene.api = {isReady:true};
       } else {
         var modulePromise = xModule.moduleReadyPromise;
@@ -226,8 +227,10 @@ AppSceneContext.prototype.runScriptInNewVMContext = function (code, uri, fromJar
         modulePromise.then( function(i) {
           self.innerscene.api = xModule.exports;
           console.log("Main module is ready - got imports: " + self.packageUrl);
+          this.container.makeReady(true);
         }).catch( function() {
           console.log("Main module load has failed - on failed imports: " + self.packageUrl);
+          this.container.makeReady(false);
         } );
       }
 
