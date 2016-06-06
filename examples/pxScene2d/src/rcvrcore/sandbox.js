6c294662500ddc87b143bce1cd26ebdc9782dd9a
hfitzpatrick <Hugh_Fitzpatrick@cable.comcast.com>
2016-06-06 14:26:28 -0400

    sandbox.js

--- examples/pxScene2d/src/rcvrcore/sandbox.js
+++ examples/pxScene2d/src/rcvrcore/sandbox.js
@@ -1,27 +1,12 @@
 
-//JUNK
-//JUNK
-//JUNK
+// console.log("\n\n############### SAND BOX stuff - Only once !\n\n");
 
-console.log("\n\n############### SAND BOX stuff - Only once !\n\n");
-
+// These vars are populated/copied into the Contexified Context... aka Clone !
+//
 var _require_it = require;
-
-// var _require_it = function (pkg)
-// {
-//   console.log("\n############### REQUIRE_IT  ");
-
-//   //log.message(3, "old use of require not supported: " + pkg);
-//   // TODO: remove
-//   return requireIt(pkg);
-// }
 
 var _urlModule         = require("url");
 var _queryStringModule = require("querystring");
 var _XModule           = require('rcvrcore/XModule').XModule;
 var _apiForChild       = this;
-
-//JUNK
-//JUNK
-//JUNK
 
