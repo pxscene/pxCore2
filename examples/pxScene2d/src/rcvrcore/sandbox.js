c1456b62e0993a52fe70fa785ef53532d8dea46c
hfitzpatrick <Hugh_Fitzpatrick@cable.comcast.com>
2016-06-06 11:42:54 -0400

    adding missing file.

---
+++ examples/pxScene2d/src/rcvrcore/sandbox.js
@@ -0,0 +1,27 @@
+
+//JUNK
+//JUNK
+//JUNK
+
+console.log("\n\n############### SAND BOX stuff - Only once !\n\n");
+
+var _require_it = require;
+
+// var _require_it = function (pkg)
+// {
+//   console.log("\n############### REQUIRE_IT  ");
+
+//   //log.message(3, "old use of require not supported: " + pkg);
+//   // TODO: remove
+//   return requireIt(pkg);
+// }
+
+var _urlModule         = require("url");
+var _queryStringModule = require("querystring");
+var _XModule           = require('rcvrcore/XModule').XModule;
+var _apiForChild       = this;
+
+//JUNK
+//JUNK
+//JUNK
+
