a2b6bbff5493c190ece28f95ada4cafb2e4175a5
hfitzpatrick <Hugh_Fitzpatrick@cable.comcast.com>
2016-06-14 05:57:12 -0400

    Adding clipboard stub to other builds

---
+++ src/wayland_egl/pxClipboardNative.h
@@ -0,0 +1,35 @@
+//
+//  pxClipboardNative.h
+//  pxScene
+//
+
+#ifndef PX_CLIPBOARDNATIVE_H
+#define PX_CLIPBOARDNATIVE_H
+
+#include <string>
+
+class pxClipboardNative
+{
+public:
+             pxClipboardNative()  {};
+    virtual ~pxClipboardNative()  {};
+    
+    
+    std::string getString(std::string type);
+           void setString(std::string type, std::string clip);
+    
+    static pxClipboardNative *instance()
+    {
+        if (!s_instance)
+        {
+            s_instance = new pxClipboardNative();
+        }
+        
+        return s_instance;
+    }
+    
+private:
+    static pxClipboardNative *s_instance;
+};
+
+#endif // PX_CLIPBOARDNATIVE_H
