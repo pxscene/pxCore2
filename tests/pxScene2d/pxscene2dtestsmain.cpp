d9611ad143c3bf6d499ae4c21dbd11f1270949c2
mthirumalai <MadanaGopal.Thirumalai@lnttechservices.com>
2017-02-06 14:21:50 -0500

    fix debug mode compilation error with unittests

--- tests/pxScene2d/pxscene2dtestsmain.cpp
+++ tests/pxScene2d/pxscene2dtestsmain.cpp
@@ -2,6 +2,12 @@
 #include "pxContext.h"
 
 pxContext context;
+#ifdef ENABLE_DEBUG_MODE
+char *g_debugPort = NULL;
+int g_argc = 0;
+char** g_argv;
+char *nodeInput = NULL;
+#endif
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
