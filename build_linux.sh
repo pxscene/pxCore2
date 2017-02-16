eeef78b2d6f04041e0eead4a0013d76430aae455
mthirumalai <madanagopal.thirumalai@lnttechservices.com>
2017-02-15 22:03:39 -0500

    Continous Integration support Phase 2 - Test runner support - Fix for errors during pull requests

--- build_linux.sh
+++ build_linux.sh
@@ -1,18 +1,58 @@
 #!/bin/sh
 BUILDLOGS=$TRAVIS_BUILD_DIR/logs/build_logs
 cd $TRAVIS_BUILD_DIR/src
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
 echo "***************************** Building pxcore and rtcore ****" >> $BUILDLOGS
 make -f Makefile.glut all>>$BUILDLOGS 2>&1;
 make -f Makefile.glut rtcore>>$BUILDLOGS 2>&1;
-echo "***************************** Building libpxscene ****" >> $BUILDLOGS;
+else
+echo "***************************** Building pxcore and rtcore ****"
+make -f Makefile.glut all 1>>$BUILDLOGS;
+make -f Makefile.glut rtcore 1>>$BUILDLOGS;
+fi
+
 cd $TRAVIS_BUILD_DIR/examples/pxScene2d/src
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+echo "***************************** Building libpxscene ****" >> $BUILDLOGS;
 make libs-glut>>$BUILDLOGS 2>&1;
+else
+echo "***************************** Building libpxscene ****";
+make libs-glut 1>>$BUILDLOGS;
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
 echo "***************************** Building pxscene app ***" >> $BUILDLOGS
 make -j>>$BUILDLOGS 2>&1
-echo "***************************** Building unittests ***" >> $BUILDLOGS;
+else
+echo "***************************** Building pxscene app ***"
+make -j 1>>$BUILDLOGS
+fi
+
 cd $TRAVIS_BUILD_DIR/tests/pxScene2d;
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+echo "***************************** Building unittests ***" >> $BUILDLOGS;
 make>>$BUILDLOGS 2>&1;
+else
+echo "***************************** Building unittests ***";
+make 1>>$BUILDLOGS;
+fi
+
 touch $TRAVIS_BUILD_DIR/logs/test_logs;
 TESTLOGS=$TRAVIS_BUILD_DIR/logs/test_logs;
 ./pxscene2dtests.sh>$TESTLOGS 2>&1;
-
+grep "FAILED" $TESTLOGS
+retVal=$?
+if [ "$retVal" -eq 0 ]
+then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+cat $TESTLOGS
+exit 1;
+fi 
+else
+exit 0;
+fi
