06768ebde2d2d5e8bc45147efba53067493c2c3f
John Robinson <johnrobinsn@gmail.com>
2017-02-19 12:45:27 -0500

    Added alpha component for color constants

--- src/pxColor.h
+++ src/pxColor.h
@@ -9,11 +9,11 @@
 
 const pxColor pxClear(0, 0, 0, 0); // RGBA
 
-const pxColor pxWhite(255, 255, 255);
-const pxColor pxBlack(0, 0, 0);
-const pxColor pxRed(255, 0, 0);
-const pxColor pxGreen(0, 255, 0);
-const pxColor pxBlue(0, 0, 255);
-const pxColor pxGray(128, 128, 128);
+const pxColor pxWhite(255, 255, 255, 255);
+const pxColor pxBlack(0, 0, 0, 255);
+const pxColor pxRed(255, 0, 0, 255);
+const pxColor pxGreen(0, 255, 0, 255);
+const pxColor pxBlue(0, 0, 255, 255);
+const pxColor pxGray(128, 128, 128, 255);
 
 #endif
