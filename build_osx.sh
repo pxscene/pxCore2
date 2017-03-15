e4efa91791b5750096998983009ce46b218193c8
mthirumalai <madanagopal.thirumalai@lnttechservices.com>
2017-03-15 14:33:40 +0530

    cache support for mac os.Ignore caching for pxcore dirs

--- build_osx.sh
+++ build_osx.sh
@@ -2,17 +2,19 @@
 BUILDLOGS=$TRAVIS_BUILD_DIR/logs/build_logs
 cd $TRAVIS_BUILD_DIR/src
 cd $TRAVIS_BUILD_DIR;
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building pxcore ****" >> $BUILDLOGS
+xcodebuild clean
 xcodebuild -scheme "pxCore Static Library">>$BUILDLOGS 2>&1;
 else
 echo "***************************** Building pxcore ****"
+xcodebuild clean
 xcodebuild -scheme "pxCore Static Library" 1>>$BUILDLOGS;
 fi
 cd $TRAVIS_BUILD_DIR/examples/pxScene2d/src;
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building pxscene app ***" >> $BUILDLOGS
 make clean;
