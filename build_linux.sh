7810afe764436604a0f9c716091a7670cc237907
mthirumalai <madanagopal.thirumalai@lnttechservices.com>
2017-03-16 10:34:31 +0530

    Enabling cache for MAC,handling intermediate script errors,ignore caching for pxcore dirs

--- build_linux.sh
+++ build_linux.sh
@@ -1,50 +1,66 @@
 #!/bin/sh
 BUILDLOGS=$TRAVIS_BUILD_DIR/logs/build_logs
+checkError()
+{
+  if [ "$1" -ne 0 ]
+  then
+  echo "Build failed with errors !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+  exit 1;
+  fi
+}
 cd $TRAVIS_BUILD_DIR/src
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building pxcore and rtcore ****" >> $BUILDLOGS
-make clean;
 make -f Makefile.glut all>>$BUILDLOGS 2>&1;
+checkError $?
 make -f Makefile.glut rtcore>>$BUILDLOGS 2>&1;
+checkError $?
 else
 echo "***************************** Building pxcore and rtcore ****"
-make clean;
 make -f Makefile.glut all 1>>$BUILDLOGS;
+checkError $?
 make -f Makefile.glut rtcore 1>>$BUILDLOGS;
+checkError $?
 fi
 
 cd $TRAVIS_BUILD_DIR/examples/pxScene2d/src
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building libpxscene ****" >> $BUILDLOGS;
 make clean;
 make libs-glut>>$BUILDLOGS 2>&1;
+checkError $?
 else
 echo "***************************** Building libpxscene ****";
 make clean;
 make libs-glut 1>>$BUILDLOGS;
+checkError $?
 fi
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building pxscene app ***" >> $BUILDLOGS
 make -j>>$BUILDLOGS 2>&1
+checkError $?
 else
 echo "***************************** Building pxscene app ***"
 make -j 1>>$BUILDLOGS
+checkError $?
 fi
 
 cd $TRAVIS_BUILD_DIR/tests/pxScene2d;
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building unittests ***" >> $BUILDLOGS;
 make clean;
 make>>$BUILDLOGS 2>&1;
+checkError $?
 else
 echo "***************************** Building unittests ***";
 make clean;
 make 1>>$BUILDLOGS;
+checkError $?
 fi
 
 touch $TRAVIS_BUILD_DIR/logs/test_logs;
--- build_osx.sh
+++ build_osx.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 BUILDLOGS=$TRAVIS_BUILD_DIR/logs/build_logs
+checkError()
+{
+  if [ "$1" -ne 0 ]
+  then
+  echo "Build failed with errors !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+  exit 1;
+  fi
+}
 cd $TRAVIS_BUILD_DIR/src
 cd $TRAVIS_BUILD_DIR;
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building pxcore ****" >> $BUILDLOGS
+xcodebuild clean
 xcodebuild -scheme "pxCore Static Library">>$BUILDLOGS 2>&1;
+checkError $?
 else
 echo "***************************** Building pxcore ****"
+xcodebuild clean
 xcodebuild -scheme "pxCore Static Library" 1>>$BUILDLOGS;
+checkError $?
 fi
 cd $TRAVIS_BUILD_DIR/examples/pxScene2d/src;
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 echo "***************************** Building pxscene app ***" >> $BUILDLOGS
 make clean;
 make -j>>$BUILDLOGS 2>&1
+checkError $?
 else
 echo "***************************** Building pxscene app ***"
 make clean;
 make -j 1>>$BUILDLOGS
+checkError $?
 fi
