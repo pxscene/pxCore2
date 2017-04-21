5b0710891c68b0d863033320e859d7bde01601bb
Michael Fiess <michael_fiess@cable.comcast.com>
2017-04-21 11:44:37 -0400

    Revert "Adding calls to Google Closure Compiler..."
    This reverts commit 270d1a3a14bd8703ab58bff46139ca95d0e994d8.

--- examples/pxScene2d/src/jsMin.sh
+++ examples/pxScene2d/src/jsMin.sh
@@ -2,8 +2,8 @@
 
 if [ "$#" -lt 1 ]
 then
-printf "\n Usage: cCompiler <input.js> <output.js> \n\n"
-exit 1
+  printf "\n Usage: cCompiler <input.js> <output.js> \n\n"
+  exit 1
 fi
 
 INPUT=$1
@@ -13,10 +13,11 @@ OUTPUT=$2
 if [ ! -e "$INPUT" ]
 then
   printf "\nCLOSURE COMPILER - Error:   INPUT file '$INPUT' not found.\n\n"
-exit 1
+  exit 1
 fi
 
-CLOSURE_COMPILER=../external/googleJScc/closure-compiler-v20170218.jar
+
+CLOSURE_COMPILER=closure-compiler-v20170218.jar
 
 # Create default output filename if needed...
 if [ -z "$2" ]
@@ -24,39 +25,9 @@ then
   OUTPUT="${INPUT%.*}".min.js
 fi
 
-
-if [[ -e $OUTPUT ]] && [[ $OUTPUT -nt $INPUT ]]
-then
-
-#  printf "\nCLOSURE COMPILER - Skipping: $INPUT  >>>  $OUTPUT  \n\n"
-  exit 0
-fi
-
-#printf "\nCLOSURE COMPILER - Process: $INPUT  >>>  $OUTPUT  \n\n"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-#LEVEL=WHITESPACE_ONLY
-LEVEL=SIMPLE_OPTIMIZATIONS
-#LEVEL=ADVANCED_OPTIMIZATIONS
-
-java -jar $CLOSURE_COMPILER --warning_level QUIET --compilation_level $LEVEL --js $INPUT --js_output_file $OUTPUT
-
-# Check input & output sizes
-#
-INPUT_size=$(stat -f%z $INPUT)
-OUTPUT_size=$(stat -f%z $OUTPUT)
-
-# Check if Minified is *actually* smaller ?  ... or copy source file
-#
-if [ $OUTPUT_size -ge $INPUT_size ]
-then
-  cp $INPUT $OUTPUT # restore ORIGINAL file ... which is smaller in this case.
-fi
+printf "\nCLOSURE COMPILER - Processing: $INPUT  >>>  $OUTPUT  \n\n"
 
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+java -jar $CLOSURE_COMPILER --js $INPUT --js_output_file $OUTPUT
 
-#printf "\n... Done !\n\n"
-
-exit 0  #success
+printf "\n... Done !\n\n"
--- examples/pxScene2d/src/mkapp.sh
+++ examples/pxScene2d/src/mkapp.sh
@@ -3,20 +3,11 @@
 externalDir=../external
 bundle=pxscene.app
 bundleBin=$bundle/Contents/MacOS
-
-#bundleRes=$bundle/Contents/Resources
-bundleRes=$bundle/Contents/MacOS
-
 bundleLib=$bundleBin/lib
 
 rm -rf $bundle
 
-# Make the bundle folder...
-#
 [ -d $bundleLib ] || mkdir -p $bundleLib
-
-# Copy LIBS to Bundle...
-#
 cp $externalDir/png/.libs/libpng16.16.dylib $bundleLib
 cp $externalDir/curl/lib/.libs/libcurl.4.dylib $bundleLib
 #cp $externalDir/libnode/out/Release/libnode.dylib $bundleLib
@@ -24,34 +15,17 @@ cp $externalDir/libnode-v6.9.0/out/Release/libnode*.dylib $bundleLib
 cp $externalDir/ft/objs/.libs/libfreetype.6.dylib $bundleLib
 cp $externalDir/zlib/libz.a $bundleLib
 
-# Copy OTHER to Bundle...
-#
 cp macstuff/Info.plist $bundle/Contents
-
-# Copy RESOURCES to Bundle...
-#
-cp -a images $bundleRes/images
-cp FreeSans.ttf $bundleRes
-cp testRunner_memcheck.js $bundleRes
-
-cp package.json $bundleRes
+cp -a images $bundleBin/images
+cp FreeSans.ttf $bundleBin
+cp browser.js $bundleBin
+cp testRunner_memcheck.js $bundleBin
+cp init.js $bundleBin
+cp shell.js $bundleBin
+cp package.json $bundleBin
 cp pxscene $bundleBin
-
 cp macstuff/pxscene.sh $bundleBin
 cp macstuff/EngineRunner $bundleBin
-
-# Minify JS into Bundle...
-#
-./jsMinFolder.sh rcvrcore $bundleRes/rcvrcore
-
-# NOTE" jsMin.sh will default to a 'min' name with 1 arg.  E.g.  "jsMin.sh INPUT.js"  >> INPUT.min.js
-#
-./jsMin.sh init.js $bundleRes/init.js
-./jsMin.sh shell.js $bundleRes/shell.js
-./jsMin.sh browser.js $bundleRes/browser.js
-
-cp -a node_modules $bundleRes/node_modules
-
-# Copy OTHER to Resources...
-#
+cp -a rcvrcore $bundleBin/rcvrcore
+cp -a node_modules $bundleBin/node_modules
 cp -a macstuff/Resources $bundle/Contents/Resources
