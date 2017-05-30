7ac1a441e43f3a7daa2b1c209924f39aebf05ffd
hfitzpatrick <Hugh_Fitzpatrick@cable.comcast.com>
2017-05-30 10:32:12 -0400

    Adding Unit tests for rtZip

---
+++ tests/pxScene2d/test_rtZip.cpp
@@ -0,0 +1,121 @@
+
+#define private public
+#define protected public
+
+#include "rtZip.h"
+#include "rtString.h"
+#include <string.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+#include "test_includes.h" // Needs to be included last
+
+using namespace std;
+
+const char  testArray[256] = {};
+const void* testBuffer = (void*) &testArray[0];
+
+class rtZipTest : public testing::Test
+{
+  public:
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+    void initFromBufferTest()
+    {
+      rtData  buffer;
+      rtError ret = rtLoadFile("supportfiles/sample.zip", buffer);
+      
+      ret = mData.initFromBuffer(buffer.data(), buffer.length());
+      EXPECT_TRUE(ret == RT_OK);
+    }
+
+    void initFromFileTest()
+    {
+      rtError ret = mData.initFromFile("supportfiles/sample.zip");
+
+      EXPECT_TRUE(ret == RT_OK);
+    }
+
+    void termTest()
+    {
+      rtError ret = mData.initFromBuffer(testBuffer, 256);
+              ret = mData.term();
+      EXPECT_TRUE(ret == RT_OK);
+    }
+
+    void fileCountTest()
+    {
+      rtError    ret = mData.initFromBuffer(testBuffer, 256);
+      uint32_t count = mData.fileCount();
+
+      EXPECT_TRUE(count == 0);
+    }
+
+    void getFilePathAtIndexTest()
+    {
+      rtError ret = mData.initFromFile("supportfiles/sample.zip");
+      EXPECT_TRUE(ret == RT_OK);
+
+      rtString path;
+
+      ret = mData.getFilePathAtIndex(-1, path);
+      EXPECT_TRUE(ret == RT_FAIL);
+
+      ret = mData.getFilePathAtIndex(0, path);
+      EXPECT_TRUE(ret == RT_OK);
+      
+      EXPECT_TRUE ( path == "test.html");  // << 'test.html' is file within 'sample.zip'
+    }
+
+    void getFileDataTest()
+    {
+      rtError ret = mData.initFromFile("supportfiles/sample.zip");
+      rtData data;
+
+      printf("\n JUNK >> ret [%d] \n", ret);
+
+      ret = mData.getFileData("does_not_exist.html", data);
+      EXPECT_TRUE(ret == RT_FAIL);
+
+      ret = mData.getFileData("test.html", data);
+      EXPECT_TRUE(ret == RT_OK);
+
+      EXPECT_TRUE(data.length() == 36);
+    }
+
+    void isZipTest()
+    {
+      rtData  buffer;
+      rtError ret = rtLoadFile("supportfiles/test.html", buffer);
+
+      EXPECT_FALSE( rtZip::isZip(buffer.data(), buffer.length()) );
+
+      ret = rtLoadFile("supportfiles/sample.zip", buffer);
+
+      EXPECT_TRUE( rtZip::isZip(buffer.data(), buffer.length()) );
+    }
+
+    private:
+      rtZip mData;
+};
+
+TEST_F(rtZipTest, rtZipTests)
+{
+  initFromBufferTest();
+  initFromFileTest();
+  termTest();
+
+  fileCountTest();
+
+  getFilePathAtIndexTest();
+  getFileDataTest();
+
+  isZipTest();
+}
+
