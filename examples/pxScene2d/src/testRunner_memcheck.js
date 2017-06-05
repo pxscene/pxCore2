1a47e8d793431e47c31ea169f0635f3f1721eee2
cfry002 <constance_fry@cable.comcast.com>
2017-06-05 13:34:44 -0700

    Remove testRunner from mem checks

--- examples/pxScene2d/src/testRunner_memcheck.js
+++ examples/pxScene2d/src/testRunner_memcheck.js
@@ -5,7 +5,7 @@ px.import("px:scene.1.js").then( function ready(scene) {
 var root = scene.root;
 
 // Info about tests to be run
-var testUrls = ["https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner.js", "http://pxscene.org/examples/px-reference/gallery/fancy.js","http://pxscene.org/examples/px-reference/gallery/picturepile.js","http://pxscene.org/examples/px-reference/gallery/gallery.js" ]; // See tests.json for test urls to be run
+var testUrls = ["http://pxscene.org/examples/px-reference/gallery/fancy.js","http://pxscene.org/examples/px-reference/gallery/picturepile.js","http://pxscene.org/examples/px-reference/gallery/gallery.js" ]; // See tests.json for test urls to be run
 
 //added different timeout for different pages, as travis server is bit slower and so getting more timeout errors for complex js pages
 var testTimeouts = [240000, 5000, 5000, 20000];
