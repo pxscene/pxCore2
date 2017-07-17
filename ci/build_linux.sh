f2a1eff4cdcf2d428e57c123bab440898bc08354
madanagopalt <madanagopal123@gmail.com>
2017-07-17 10:35:55 -0400

    Update build_linux.sh

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -21,17 +21,9 @@ echo "***************************** Generating config files ****" >> $BUILDLOGS
 cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON . >>$BUILDLOGS 2>&1;
 checkError $? "cmake config failed" "Config error" "Check the error in $BUILDLOGS"
 
-echo "***************************** Building pxcore and rtcore ****" >> $BUILDLOGS
-cmake --build src >>$BUILDLOGS 2>&1;
+echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene, unitttests ****" >> $BUILDLOGS
+cmake . >>$BUILDLOGS 2>&1;
 checkError $? "cmake build failed for pxcore or rtcore" "Compilation error" "Check the error in $BUILDLOGS"
-
-echo "***************************** Building pxscene app and libpxscene ****" >> $BUILDLOGS;
-cmake --build examples/pxScene2d/src >>$BUILDLOGS 2>&1;
-checkError $? "cmake build failed or pxscene app or libpxscene" "Compilation error" "Check the error in $BUILDLOGS"
-
-echo "***************************** Building unittests ***" >> $BUILDLOGS;
-cmake --build tests/pxScene2d >>$BUILDLOGS 2>&1;
-checkError $? "cmake build failed for unittests" "Compilation error" "Check the error in $BUILDLOGS"
 
 else
 echo "***************************** Generating config files ****"
@@ -39,15 +31,8 @@ cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON . 1>>$BUILDLOGS;
 checkError $? "cmake config failed" "Config error" "Check the errors displayed in this window"
 cat $BUILDLOGS
 
-echo "***************************** Building pxcore and rtcore ****"
-cmake --build src 1>>$BUILDLOGS;
+echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene, unitttests ****"
+cmake . 1>>$BUILDLOGS;
 checkError $? "cmake build failed for pxcore or rtcore" "Compilation error" "Check the errors displayed in this window"
 
-echo "***************************** Building pxscene app and libpxscene ****";
-cmake --build examples/pxScene2d/src/ 1>>$BUILDLOGS;
-checkError $? "cmake build failed or pxscene app or libpxscene" "Compilation error" "Check the errors displayed in this window"
-
-echo "***************************** Building unittests ***";
-cmake --build tests/pxScene2d/ 1>>$BUILDLOGS;
-checkError $? "cmake build failed for unittests" "Compilation error" "Check the errors displayed in this window"
 fi
