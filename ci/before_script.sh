4520a9e6e6f505f94e911b5b34d136e923690c87
madanagopalt <madanagopal123@gmail.com>
2017-07-20 09:52:18 -0400

    Update before_script.sh

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-
 #make arrangements for ignoring pxwayland tests for osx and linux
-#if [ "$TRAVIS_OS_NAME" = "osx" ]
-#then
 currdir = `pwd`
 cd $TRAVIS_BUILD_DIR/tests/pxScene2d/testRunner
 cp tests.json tests.json_orig
+if [ "$TRAVIS_OS_NAME" = "osx" ]
+then
 sed -i -n '/pxWayland/d' tests.json
+else
+sed -i '/pxWayland/d' tests.json
+fi
 cd $currdir
-#fi
 exit 0;
