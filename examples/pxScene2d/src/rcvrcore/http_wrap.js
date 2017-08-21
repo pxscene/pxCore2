fa65f5768c809fe128373681ebb0d37cc41bb275
madanagopalt <madanagopal123@gmail.com>
2017-08-21 17:29:31 +0530

    disabling local http and https access for remote files

--- examples/pxScene2d/src/rcvrcore/http_wrap.js
+++ examples/pxScene2d/src/rcvrcore/http_wrap.js
@@ -1,13 +1,119 @@
 'use strict';
+
 var http = require('http');
 
-module.exports.IncomingMessage = http.IncomingMessage;
-module.exports.Agent = http.Agent;
-module.exports.METHODS = http.METHODS;
-module.exports.OutgoingMessage = http.OutgoingMessage;
-module.exports.globalAgent = http.globalAgent;
-module.exports.ClientRequest = http.ClientRequest;
-module.exports.request = http.request;
-module.exports.get = http.get;
-module.exports.Client = http.Client;
-module.exports.createClient = http.createClient;
+function isLocalAccess(reqOptions)
+{
+  if (((reqOptions.hostname) && ((reqOptions.hostname === "localhost") || (reqOptions.hostname === "127.0.0.1"))) || ((reqOptions.host) && ((reqOptions.host === "localhost") || (reqOptions.host === "127.0.0.1"))))
+  {
+    return true;
+  }
+  else if (((reqOptions.hostname) && ((reqOptions.hostname === "[::1]") || (reqOptions.hostname === "[0:0:0:0:0:0:0:1]"))) || ((reqOptions.host) && ((reqOptions.host === "[::1]") || (reqOptions.host === "[0:0:0:0:0:0:0:1]"))))
+  {
+    return true;
+  }
+  return false;
+}
+
+function HttpWrap()
+{
+  this.localApp = false;
+}
+
+HttpWrap.prototype.IncomingMessage = http.IncomingMessage;
+HttpWrap.prototype.METHODS = http.METHODS;
+HttpWrap.prototype.OutgoingMessage = http.OutgoingMessage;
+
+HttpWrap.prototype.setLocalApp = function(isLocalApp) {
+  this.localApp = isLocalApp;
+};
+
+HttpWrap.prototype.getLocalApp = function() {
+  return this.localApp;
+};
+
+HttpWrap.prototype.request = function(options, cb) {
+  if (true == isLocalAccess(options))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.request(options, cb);
+};
+
+HttpWrap.prototype.ClientRequest = function(options, cb) {
+  if (true == isLocalAccess(options))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.ClientRequest(options, cb);
+};
+
+HttpWrap.prototype.get = function(options, cb) {
+  if (true == isLocalAccess(options))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.get(options, cb);
+};
+
+HttpWrap.prototype.Agent = function(options) {
+  if (true == isLocalAccess(options))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.Agent(options);
+};
+
+HttpWrap.prototype.globalAgent = function(options) {
+  if (true == isLocalAccess(options))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.globalAgent(options);
+};
+
+HttpWrap.prototype.Client = function(port, host) {
+  if ((host === "localhost") || (host === "127.0.0.1"))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.Client(port,host);
+};
+
+HttpWrap.prototype.createClient = function(port, host) {
+  if ((host === "localhost") || (host === "127.0.0.1"))
+  {
+    if (false == this.localApp)
+    {
+      console.log("localhost urls cannot be accessed by remote applications");
+      return;
+    }
+  }
+  return http.createClient(port,host);
+};
+
+module.exports = HttpWrap;
