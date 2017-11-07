caf37d04158c87d9ffc1beb207142751afd6ccfd
conniefry <constance_fry@cable.comcast.com>
2017-11-07 11:30:56 -0800

    reinstate coverity scan scripts and travis

--- ci/clean_coverity.sh
+++ ci/clean_coverity.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+<<<<<<< HEAD
 cd $TRAVIS_BUILD_DIR 
 rm -rf temp
+=======
+cd $TRAVIS_BUILD_DIR/src
+make -f Makefile.glut clean
+cd $TRAVIS_BUILD_DIR/examples/pxScene2d/src
+make clean
+>>>>>>> reinstate coverity scan scripts and travis
