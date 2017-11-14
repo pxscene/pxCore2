4412459bb872c7c65e41e020d6631ec135270ea8
arun-govindan <arun.govindan@lnttechservices.com>
2017-11-14 15:12:10 +0530

    Fixed review comments

--- tests/pxScene2d/test_pxTimerNative.cpp
+++ tests/pxScene2d/test_pxTimerNative.cpp
@@ -1,10 +1,9 @@
 #include "pxTimer.h"
-
 #include "test_includes.h" // Needs to be included last
 
 using namespace std;
 
-#define TEST_TIME 8
+#define TEST_TIME 2
 
 class pxTimerNativeTest : public testing::Test
 {
@@ -19,10 +18,23 @@ class pxTimerNativeTest : public testing::Test
 
     void testpxTimerNative()
     {
-        pxSeconds();
-        pxMilliseconds();
-        pxMicroseconds();
-        pxSleepMS(TEST_TIME);
+        double startTime = 0.0;
+        double endTime = 0.0;
+        double opTime = 0.0;
+        
+        opTime = pxSeconds();
+        EXPECT_TRUE(opTime > 0);
+ 
+        opTime = pxMilliseconds();
+        EXPECT_TRUE(opTime > 0);
+        
+        opTime = pxMicroseconds();
+        EXPECT_TRUE(opTime > 0);
+ 
+        startTime = pxSeconds();
+        pxSleepMS(TEST_TIME * 1000);
+        endTime = pxSeconds();
+        EXPECT_TRUE((int)(endTime - startTime) == TEST_TIME);
     }
 
 };
