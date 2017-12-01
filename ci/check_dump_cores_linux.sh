dfc962180443eef07cbcf021a99037d737e644ba
Govindan <arun.govindan@lnttechservices.com>
2017-12-01 10:18:45 -0500

    intendation done
    Conflicts:
    	ci/execute_linux.sh
    	ci/execute_osx.sh

--- ci/check_dump_cores_linux.sh
+++ ci/check_dump_cores_linux.sh
@@ -8,25 +8,25 @@ cd $path
 ls -lrt core
 retVal=$?
 if [ "$retVal" -eq 0 ]
-then
-tempLogFile="gdb.txt"
-coreFileName="core"
-sudo chmod 777 $coreFileName
-echo "gdb -q --command=\"$TRAVIS_BUILD_DIR/ci/debuggercmds_linux\" -c $coreFileName $binary  2&>gdblogs"
-gdb -q --command="$TRAVIS_BUILD_DIR/ci/debuggercmds_linux" -c $coreFileName $binary  2&>gdblogs
-cat gdblogs
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-echo "Core stack details ---------------------------------------- from $path"
-cat $tempLogFile
+	then
+	tempLogFile="gdb.txt"
+	coreFileName="core"
+	sudo chmod 777 $coreFileName
+	echo "gdb -q --command=\"$TRAVIS_BUILD_DIR/ci/debuggercmds_linux\" -c $coreFileName $binary  2&>gdblogs"
+	gdb -q --command="$TRAVIS_BUILD_DIR/ci/debuggercmds_linux" -c $coreFileName $binary  2&>gdblogs
+	cat gdblogs
+	if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+		then
+		echo "Core stack details ---------------------------------------- from $path"
+		cat $tempLogFile
+	else
+		echo "Core stack details ---------------------------------------- from $path" >> $logfile
+		cat "$tempLogFile" >> $logfile
+	fi
+	rm -rf "$tempLogFile"
+	rm -rf gdblogs
+	exit 1;
 else
-echo "Core stack details ---------------------------------------- from $path" >> $logfile
-cat "$tempLogFile" >> $logfile
-fi
-rm -rf "$tempLogFile"
-rm -rf gdblogs
-exit 1;
-else
-echo "No core files generated in $path"
+	echo "No core files generated in $path"
 fi
 exit 0;
