6387ddde0a64ac0ea33f7e5326563c1367f04ddd
madanagopalt <madanagopal123@gmail.com>
2017-12-11 17:23:46 +0530

    close pending websocket connections on page reloads

--- examples/pxScene2d/src/rcvrcore/ws_wrap.js
+++ examples/pxScene2d/src/rcvrcore/ws_wrap.js
@@ -1,11 +1,41 @@
 'use strict';
-var WS = module.exports = require('ws/lib/WebSocket');
-WS.Sender = require('ws/lib/Sender');
-WS.Receiver = require('ws/lib/Receiver');
-WS.connect = WS.createConnection = function connect(address, fn) {
-  var client = new WS(address);
+
+var WebSocket = require('ws/lib/WebSocket');
+
+function WebSocketWrap()
+{
+  this.connections = new Array();
+}
+
+WebSocketWrap.prototype.clearConnections = function() {
+  var connectionLength = this.connections.length;
+  for (var i = 0; i < connectionLength; i++) {
+    this.connections[i].close();
+  };
+  this.connections.splice(0, this.connections.length);
+  delete this.connections;
+  this.conections = null;
+}
+
+WebSocketWrap.prototype.connect = WebSocketWrap.prototype.createConnection = function(address, fn) {
+  var client = new WebSocket(address);
   if (typeof fn === 'function') {
     client.on('open', fn);
   }
+  this.connections.push(client);
   return client;
 };
+
+WebSocketWrap.prototype.WebSocket = function (address, protocols, options) {
+  var client = new WebSocket(address, protocols, options);
+  if (typeof fn === 'function') {
+    client.on('open', fn);
+  }
+  this.connections.push(client);
+  return client;
+};
+
+//export to client
+var WebSocketExport = module.exports;
+WebSocketExport.WebSocket = WebSocketWrap;
+module.exports = WebSocketExport;
