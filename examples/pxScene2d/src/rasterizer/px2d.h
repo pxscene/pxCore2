542325ec344935a5b994d3295413988e9befd441
Andrew Benton <Andrew.Benton675@gmail.com>
2017-12-15 21:43:28 -0800

    Clang diagnostics for all platforms
    Previously, the clang diagnostic pragma for registers was only used on
    mac.  On modern linux version of clang, this causes the build to fail.

--- examples/pxScene2d/src/rasterizer/px2d.h
+++ examples/pxScene2d/src/rasterizer/px2d.h
@@ -1,5 +1,5 @@
 
-#ifdef PX_PLATFORM_MAC
+#ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-register"
 #pragma clang diagnostic ignored "-Wconversion"
@@ -46,8 +46,8 @@ inline void pxLerp2(uint32_t a, uint32_t &d, const uint32_t s)
 	uint32 drb = srcrb - dstrb;
 	uint32 dag = srcag - dstag;
 
-	drb *= a;  
-	dag *= a;  
+	drb *= a;
+	dag *= a;
 	drb >>= 8;
 	dag >>= 8;
 
@@ -72,7 +72,7 @@ inline void pxLerp2(uint32 a, uint32 &d, const uint32 s)
 	uint32 dg  =  srcg - dstg;
 
 	drb *= a;
-	dg  *= a;  
+	dg  *= a;
 	drb >>= 8;
 	dg  >>= 8;
 
@@ -98,7 +98,7 @@ inline void pxBlend(uint32 &d, const uint32 s)
 	uint32 dg  =  srcg - dstg;
 
 	drb *= a;
-	dg  *= a;  
+	dg  *= a;
 	drb >>= 8;
 	dg  >>= 8;
 
@@ -110,7 +110,7 @@ inline void pxBlend(uint32 &d, const uint32 s)
 }
 
 // uses dst alpha - preserves alpha
-inline bool pxBlendBehind (  
+inline bool pxBlendBehind (
   uint32& src1, uint32 dst1)
 {
 #define _agmask					0xFF00FF00
@@ -118,18 +118,18 @@ inline bool pxBlendBehind (
 
   uint32 da = src1>>24;
 
-  if (da >= 255) 
+  if (da >= 255)
     return 1;
 
   uint32 sa = dst1>>24;
-  
+
   register		uint32 alpha1	= 256-da;
 	register		uint32 agd1		= (dst1&0xff00)>>8;
 	register		uint32 rbd1		=  dst1&_rbmask;
 
 	register		uint32 ags1		=  src1&_agmask;
 	register		uint32 rbs1		=  src1&_rbmask;
-  
+
 #if 1
   rbd1 *= sa;
   rbd1 >>= 8;
@@ -140,7 +140,7 @@ inline bool pxBlendBehind (
   agd1 &= 0xff;
   agd1 |= (sa << 16);
 
-  agd1 							= ags1 +  (agd1*alpha1);									
+  agd1 							= ags1 +  (agd1*alpha1);
 	rbd1							= rbs1 + ((rbd1*alpha1)>>8);
 
 #else
@@ -153,25 +153,25 @@ inline bool pxBlendBehind (
   agd1 &= 0xff;
   agd1 |= (alpha1 << 16);
 
-  agd1 							= ags1 +  (agd1*sa);									
+  agd1 							= ags1 +  (agd1*sa);
 	rbd1							= rbs1 + ((rbd1*sa)>>8);
 
 #endif
 
-	
+
 	src1 = (agd1&_agmask) | (rbd1&_rbmask);
   return 0;
 }
 
 // Assumes premultiplied
-inline bool pxPreMultipliedBlendBehind (  
+inline bool pxPreMultipliedBlendBehind (
   uint32_t& src1, uint32_t dst1)
 {
 //#define _agmask					0xFF00FF00
 //#define _rbmask					0x00FF00FF
   uint32 a = src1>>24;
 #if 1
-  if (a >= 255) 
+  if (a >= 255)
     return 1;
 #endif
 	register		uint32_t alpha1	= 256-a;
@@ -179,38 +179,38 @@ inline bool pxPreMultipliedBlendBehind (
 	register		uint32_t rbd1		=  dst1&_rbmask;
 	register		uint32_t ags1		=  src1&_agmask;
 	register		uint32_t rbs1		=  src1&_rbmask;
-	agd1 							= ags1 +  (agd1*alpha1);									
+	agd1 							= ags1 +  (agd1*alpha1);
 	rbd1							= rbs1 + ((rbd1*alpha1)>>8);
-	
+
 	src1 = (agd1&_agmask) | (rbd1&_rbmask);
   return 0;
 }
 
 
-inline pxPixel pxBlend4(const pxPixel& s1, const pxPixel& s2, 
-                        const pxPixel& s3, const pxPixel& s4, 
+inline pxPixel pxBlend4(const pxPixel& s1, const pxPixel& s2,
+                        const pxPixel& s3, const pxPixel& s4,
                         const uint32_t xp, const uint32_t yp)
 {
 #define _rbmask2 0xFF00FF
 //#define _agmask2 0xFF00
 #define _agmask2 0xFF00FF00
 
-    
+
   uint32 arb = s1.u & _rbmask2;
   uint32 brb = s2.u & _rbmask2;
   uint32 crb = s3.u & _rbmask2;
   uint32 drb = s4.u & _rbmask2;
-    
+
   uint32 aag = s1.u & _agmask2;
   uint32 bag = s2.u & _agmask2;
   uint32 cag = s3.u & _agmask2;
   uint32 dag = s4.u & _agmask2;
-	
+
   uint32 rbdx1 = (brb     ) - (arb     );
   uint32 rbdx2 = (drb     ) - (crb     );
   uint32 agdx1 = (bag >> 8) - (aag >> 8);
   uint32 agdx2 = (dag >> 8) - (cag >> 8);
-    
+
   uint32 rb1 = (arb + ((rbdx1 * xp) >> 8)) & _rbmask2;
   uint32 rb2 = (crb + ((rbdx2 * xp) >> 8)) & _rbmask2;
   uint32 ag1 = (aag + ((agdx1 * xp)     )) & _agmask2;
@@ -218,10 +218,10 @@ inline pxPixel pxBlend4(const pxPixel& s1, const pxPixel& s2,
 
   uint32 rbdy = (rb2     ) - (rb1     );
   uint32 agdy = (ag2 >> 8) - (ag1 >> 8);
-    
+
   uint32 rb = (rb1 + ((rbdy * yp) >> 8)) & _rbmask2;
   uint32 ag = (ag1 + ((agdy * yp)     )) & _agmask2;
-    
+
   return pxPixel(ag | rb);
 }
 
