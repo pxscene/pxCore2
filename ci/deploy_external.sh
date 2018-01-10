12c1c48d86baa668696693a6991d58672db7c185
arun-govindan <arun.govindan@lnttechservices.com>
2018-01-10 14:39:47 -0500

    test mac

--- ci/deploy_external.sh
+++ ci/deploy_external.sh
@@ -4,15 +4,15 @@ export DEPLOY_USER="${DEPLOY_USER:-ubuntu}"
 REMOTE_HOST="$1"
 #REMOTE_DIR="${DEPLOY_DESTINATION}/external"
 filename=$2
+export REMOTE_FILE_COUNT=$(ssh -o StrictHostKeyChecking=no -p 2220 ${DEPLOY_USER}@${REMOTE_HOST} "ls -lrt $DEPLOY_DESTINATION|grep "external" |wc -l")
+
+export REMOTE_FILE_OLD=$(ssh -o StrictHostKeyChecking=no -p 2220 ${DEPLOY_USER}@${REMOTE_HOST} "ls -t $DEPLOY_DESTINATION|grep "external"|tail -1")
 scp -P 2220 ${filename} ${DEPLOY_USER}@${REMOTE_HOST}:${DEPLOY_DESTINATION}
 if [ "$?" -ne 0 ]
 then
   echo "******************scp failed******************"
   exit 1;	
 fi
-echo "***********************trace***********"
-export REMOTE_FILE_COUNT=$(ssh -o StrictHostKeyChecking=no -p 2220 ${DEPLOY_USER}@${REMOTE_HOST} "ls -lrt $DEPLOY_DESTINATION|grep "external" |wc -l")
-export REMOTE_FILE_OLD=$(ssh -o StrictHostKeyChecking=no -p 2220 ${DEPLOY_USER}@${REMOTE_HOST} "ls -t $DEPLOY_DESTINATION|grep "external"|tail -1")
 SSH="ssh -tt -o StrictHostKeyChecking=no -p 2220 -l ${DEPLOY_USER} ${REMOTE_HOST}"
 $SSH "set -e;
 if [ $REMOTE_FILE_COUNT -gt 1 ]
