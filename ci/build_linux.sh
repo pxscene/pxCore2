242903d3de5740d19981828106c6fafbca0449ef
arun-govindan <arun.govindan@lnttechservices.com>
2018-01-19 10:46:02 -0500

    updated ci scripts

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -4,13 +4,13 @@ checkError()
 {
   if [ "$1" -ne 0 ]
   then
-        echo "*********************************************************************";
-        echo "*********************SCRIPT FAIL DETAILS*****************************";	
-	      echo "CI failure reason: $2"
-	      echo "Cause: $3"
-	      echo "Reproduction/How to fix: $4"
-        echo "*********************************************************************";
-        echo "*********************************************************************";
+        printf "\n\n*********************************************************************";
+        printf "\n******************** SCRIPT FAIL DETAILS ****************************";	
+	printf "\nCI failure reason: $2"
+	printf "\nCause: $3"
+	printf "\nReproduction/How to fix: $4"
+        printf "\n*********************************************************************";
+        printf "\n*********************************************************************\n\n";
 	exit 1;
   fi
 }
@@ -21,20 +21,20 @@ mkdir temp
 cd temp
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-	echo "***************************** Generating config files ****" >> $BUILDLOGS
+	echo "************************* Generating config files *************************" >> $BUILDLOGS
 	cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. >>$BUILDLOGS 2>&1;
 	checkError $? "cmake config failed" "Config error" "Check the error in $BUILDLOGS"
 
-	echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene, unitttests ****" >> $BUILDLOGS
+	echo "******** Building pxcore,rtcore,pxscene app,libpxscene, unitttests ********" >> $BUILDLOGS
 	cmake --build . --clean-first -- -j1 >>$BUILDLOGS 2>&1;
 	checkError $? "cmake build failed for pxcore or rtcore" "Compilation error" "Check the error in $BUILDLOGS"
 
 else
-	echo "***************************** Generating config files ****"
+	echo "************************* Generating config files *************************"
 	cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. 1>>$BUILDLOGS;
 	checkError $? "cmake config failed" "Config error" "Check the errors displayed in this window"
 
-	echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene, unitttests ****"
+	echo "******** Building pxcore,rtcore,pxscene app,libpxscene, unitttests ********"
 	cmake --build . --clean-first -- -j1 1>>$BUILDLOGS;
 	checkError $? "cmake build failed for pxcore,rtcore,pxscene app,libpxscene or unitttests" "Compilation error" "Check the errors displayed in this window"
 
