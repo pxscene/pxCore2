0bf01a4d59503db5fcc2593c327e79ae1986ab62
arun-govindan <30860944+arun-govindan@users.noreply.github.com>
2018-01-22 08:35:22 -0500

    updated ci scripts (#801)

--- ci/build_osx.sh
+++ ci/build_osx.sh
@@ -11,18 +11,18 @@ checkError()
 {
 if [ "$1" -ne 0 ]
 then
-	echo "*********************************************************************";
-  echo "**********************BUILD FAIL DETAILS*****************************";
-	echo "CI failure reason: $3"
-	echo "Cause: $4"
-	echo "Reproduction/How to fix: $5"
-	echo "*********************************************************************";
-	echo "*********************************************************************";
+	printf "\n\n*********************************************************************";
+        printf "\n********************* BUILD FAIL DETAILS ****************************";
+	printf "\nCI failure reason: $3"
+	printf "\nCause: $4"
+	printf "\nReproduction/How to fix: $5"
+	printf "\n*********************************************************************";
+	printf "\n*********************************************************************\n\n";
 	if [ "$2" -eq 1 ]
 		then
-		echo "**************************PRINTING BUILD LOGS****************************";
+		printf "\n\n************************ PRINTING BUILD LOGS *************************";
 		cat $BUILDLOGS
-		echo "*************************************************************************";
+		printf "\n**********************************************************************\n\n";
 	fi
 exit 1;
 fi
@@ -39,7 +39,7 @@ cd  temp
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 
-	echo "***************************** Generating config files ****" >> $BUILDLOGS
+	echo "************************* Generating config files *************************" >> $BUILDLOGS
 	if [ "$TRAVIS_EVENT_TYPE" != "cron" ] && [ "$TRAVIS_EVENT_TYPE" != "api" ] ;
 	then
 		cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. >>$BUILDLOGS 2>&1;
@@ -47,16 +47,16 @@ then
 		cmake .. >>$BUILDLOGS 2>&1;
 	fi
 	checkError $? 0 "cmake config failed" "Config error" "Check the error in $BUILDLOGS"
-	echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene,unitttests ****" >> $BUILDLOGS
+	echo "********* Building pxcore,rtcore,pxscene app,libpxscene,unitttests ********" >> $BUILDLOGS
 	cmake --build . -- -j1 >>$BUILDLOGS 2>&1;
 	checkError $? 0 "Building either pxcore,rtcore,pxscene app,libpxscene,unitttest failed" "Compilation error" "check the $BUILDLOGS file"
 
 else
 
-	echo "***************************** Generating config files ****"
+	echo "************************* Generating config files *************************"
 	cmake -DBUILD_PX_TESTS=ON -DBUILD_PXSCENE_STATIC_LIB=ON -DBUILD_DEBUG_METRICS=ON .. 1>>$BUILDLOGS;
 	checkError $? 1  "cmake config failed" "Config error" "Check the errors displayed in this window"
-	echo "***************************** Building pxcore,rtcore,pxscene app,libpxscene,unitttests ****" >> $BUILDLOGS
+	echo "******** Building pxcore,rtcore,pxscene app,libpxscene,unitttests *********" >> $BUILDLOGS
 	cmake --build . -- -j1 1>>$BUILDLOGS;
 	checkError $? 1 "Building either pxcore,rtcore,pxscene app,libpxscene,unitttest failed" "Compilation error" "Check the errors displayed in this window"
 fi
@@ -64,7 +64,7 @@ fi
 cd $TRAVIS_BUILD_DIR
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 	then
-	echo "***************************** Building pxscene deploy app ***" >> $BUILDLOGS
+	echo "*********************** Building pxscene deploy app ***********************" >> $BUILDLOGS
 
 	if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ] ;
 		then
--- ci/execute_osx.sh
+++ ci/execute_osx.sh
@@ -4,13 +4,13 @@ checkError()
 {
   if [ "$1" -ne 0 ]
   then
-        echo "*********************************************************************";
-        echo "*********************BUILD FAIL DETAILS******************************";
-        echo "CI failure reason: $2"
-        echo "Cause: $3"
-        echo "Reproduction/How to fix: $4"
-        echo "*********************************************************************";
-        echo "*********************************************************************";
+        printf "\n\n*********************************************************************";
+        printf "\n*********************BUILD FAIL DETAILS******************************";
+        printf "\nCI failure reason: $2"
+        printf "\nCause: $3"
+        printf "\nReproduction/How to fix: $4"
+        printf "\n*********************************************************************";
+        printf "\n*********************************************************************\n\n";
         exit 1;
   fi
 }
@@ -106,10 +106,10 @@ else
 	if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 		then
 		errCause="Check the above logs"
-		echo "**********************PRINTING EXEC LOG**************************"
-   		cat $EXECLOGS
-    		echo "**************************LOG ENDS*******************************"
-	else
+	        echo "********************** PRINTING EXEC LOG **************************"
+                cat $EXECLOGS
+                echo "************************** LOG ENDS *******************************"
+        else
 		errCause="Check the $EXECLOGS file"
 	fi 
 	checkError -1 "Texture leak or pxobject leak" "$errCause" "Follow steps locally: export PX_DUMP_MEMUSAGE=1;export RT_LOG_LEVEL=info;./pxscene.sh $TESTRUNNERURL?tests=<pxcore dir>/tests/pxScene2d/testRunner/tests.json locally and check for 'texture memory usage is' and 'pxobjectcount is' in logs and see which is non-zero" 
@@ -122,10 +122,10 @@ if [ "$leakcount" -ne 0 ]
 	if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 		then
 		errCause="Check the above logs"
-		echo "**********************PRINTING LEAK LOG**************************"
+                echo "********************** PRINTING EXEC LOG **************************"
                 cat $LEAKLOGS
-                echo "**************************LOG ENDS*******************************"
-	else
+	        echo "************************** LOG ENDS *******************************"
+        else
 		errCause="Check the file $LEAKLOGS and $EXECLOGS"
 	fi
 	checkError $leakcount "Execution reported memory leaks" "$errCause" "Run locally with these steps: export ENABLE_MEMLEAK_CHECK=1;export MallocStackLogging=1;export PX_DUMP_MEMUSAGE=1;./pxscene.sh $TESTRUNNERURL?tests=<pxcore dir>/tests/pxScene2d/testRunner/tests.json &; run leaks -nocontext pxscene >logfile continuously until the testrunner execution completes; Analyse the logfile" 
