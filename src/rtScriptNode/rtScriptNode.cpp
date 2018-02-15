8bf119ae4d3d122f183abe84e4115f6051513251
mthirumalai <madanagopal_thirumalai@comcast.com>
2018-02-15 13:12:49 +0530

    fix for executing promise resolved immediately

--- src/rtScriptNode/rtScriptNode.cpp
+++ src/rtScriptNode/rtScriptNode.cpp
@@ -1042,6 +1042,7 @@ rtError rtScriptNode::pump()
   v8::platform::PumpMessageLoop(mPlatform, mIsolate);
 #endif //ENABLE_NODE_V_6_9
   uv_run(uv_default_loop(), UV_RUN_NOWAIT);//UV_RUN_ONCE);
+  mIsolate->RunMicrotasks();
 
   // Enable this to expedite garbage collection for testing... warning perf hit
   if (mTestGc)
