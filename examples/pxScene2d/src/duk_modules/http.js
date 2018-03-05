08eb3eae0c16ebd3a133b922d402acc8578c30a8
Justin Gasper <jmgasper@gmail.com>
2018-03-06 09:46:49 +1100

    Websocket support

--- examples/pxScene2d/src/duk_modules/http.js
+++ examples/pxScene2d/src/duk_modules/http.js
@@ -4,6 +4,11 @@ function get(url, cb) {
     return httpGet(url, cb);
 }
 
+function request(options) {
+    return _httpRequest(options);
+}
+
 module.exports = {
     'get': get,
+    'request': request,
 }
