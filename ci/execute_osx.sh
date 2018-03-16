ec1ddd73b233eb504523339458d9c29dfd309c0b
mfiess <michael_fiess@cable.comcast.com>
2018-03-16 11:22:50 -0400

    use testRunner_v2

--- ci/execute_osx.sh
+++ ci/execute_osx.sh
@@ -122,7 +122,7 @@ if [ "$retVal" -ne 0 ]
         else
 		errCause="Either one or more tests failed. Check the log file $EXECLOGS"
 	fi
-	checkError $retVal "Testrunner execution failed" "$errCause" "Run pxscene with testrunner.js locally as ./pxscene.sh https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner_v2?tests=<pxcore dir>tests/pxScene2d/testRunner/tests.json"
+	checkError $retVal "Testrunner execution failed" "$errCause" "Run pxscene with testrunner.js locally as ./pxscene.sh https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner_v2.js?tests=<pxcore dir>tests/pxScene2d/testRunner/tests.json"
 	exit 1;
 fi
 
