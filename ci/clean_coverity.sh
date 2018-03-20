a5ace9b8b0024e23d085527210644ed536d7bea1
Connie Fry <conniefry@users.noreply.github.com>
2018-03-20 16:10:31 -0700

    Fix clean_coverity script

--- ci/clean_coverity.sh
+++ ci/clean_coverity.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 cd $TRAVIS_BUILD_DIR 
 rm -rf temp
-=======
-cd $TRAVIS_BUILD_DIR/src
-make -f Makefile.glut clean
-cd $TRAVIS_BUILD_DIR/examples/pxScene2d/src
-make clean
->>>>>>> reinstate coverity scan scripts and travis
-=======
-cd $TRAVIS_BUILD_DIR 
-rm -rf temp
->>>>>>> Update for cmake (#873)
-=======
-cd $TRAVIS_BUILD_DIR 
-rm -rf temp
->>>>>>> adfea4129d0122c853197b89752863044629b20b
