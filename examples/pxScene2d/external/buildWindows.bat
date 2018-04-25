1a7fd6c8608515b24b68977c99b4e0e8b5a826bb
Govindan <arun.govindan@lnttechservices.com>
2018-04-25 10:43:55 -0400

    Cached external libs

--- examples/pxScene2d/external/buildWindows.bat
+++ examples/pxScene2d/external/buildWindows.bat
@@ -6,15 +6,36 @@
  pause
  goto :eof
 )
+@echo off
+setlocal enabledelayedexpansion
 
 copy /y libjpeg-turbo-1.5.1\win_temp\* libjpeg-turbo-1.5.1\
 copy /y curl-7.40.0\include\curl\curlbuild-win.h curl-7.40.0\include\curl\curlbuild.h
 copy /y libpng-1.6.28\scripts\pnglibconf.h.prebuilt libpng-1.6.28\pnglibconf.h
 copy /y jpeg-9a\jconfig.vc jpeg-9a\jconfig.h
 
-cd vc.build\
-msbuild external.sln /p:Configuration=Release /p:Platform=Win32 /m
+set buildExternal=0
+FOR /F "tokens=* USEBACKQ" %%F IN (`git diff-tree --name-only --no-commit-id -r %APPVEYOR_REPO_COMMIT%`) DO (
+ echo.%%F|findstr "zlib-1.2.11 WinSparkle pthread-2.9 libpng-1.6.28 libjpeg-turbo-1.5.1 glew-2.0.0 freetype-2.5.2 curl-7.40.0 jpeg-9a"
+  if !errorlevel! == 0 (
+    set buildExternal=1
+    echo. External library files are modified. Need to build external : !buildExternal! .
+    GOTO BREAK_LOOP1
+  )
+)
+:BREAK_LOOP1
+cd vc.build
+if NOT EXIST builds (
+  set buildExternal=1
+  echo Cache not available. Need to build external : !buildNeeded!.
+)
 cd ..
+if %buildExternal% == 1 (
+  echo. Building external library  : %cd%
+  cd vc.build\
+  msbuild external.sln /p:Configuration=Release /p:Platform=Win32 /m
+  cd ..
+)
 
 cd breakpad-chrome_55
 CALL gyp\gyp.bat src\client\windows\breakpad_client.gyp --no-circular-check
