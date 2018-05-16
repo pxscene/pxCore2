645358e3ba7fe97ba5ba5926ba10894292de6cf0
kannanmadanagopalt <kannan220884@gmail.com>
2018-05-16 11:50:26 +0530

    Update AppSceneContext.js

--- examples/pxScene2d/src/rcvrcore/AppSceneContext.js
+++ examples/pxScene2d/src/rcvrcore/AppSceneContext.js
@@ -109,9 +109,6 @@ if( fullPath !== null)
   this.loadPackage(fullPath);
 
 this.innerscene.on('onSceneTerminate', function (e) {
-    if (!isDuk) {
-      process._tickCallback();
-    }
     if (null != this.webSocketManager)
     {
        this.webSocketManager.clearConnections();
