2b0aa8379a374927369182fb1fbe9c3326304ef9
mthirumalai <madanagopal_thirumalai@comcast.com>
2018-05-19 07:41:55 +0530

    Revert "revert debug prints"
    This reverts commit 4ce6da9ad29b84f0cfb93ead85d75689f0679da0.

--- examples/pxScene2d/src/pxScene2d.cpp
+++ examples/pxScene2d/src/pxScene2d.cpp
@@ -1,3 +1,4 @@
+
 /*
 
  pxCore Copyright 2005-2018 John Robinson
@@ -549,6 +550,8 @@ pxObject::pxObject(pxScene2d* scene): rtObject(), mParent(NULL), mcx(0), mcy(0),
 #endif //PX_DIRTY_RECTANGLES
     ,mDrawableSnapshotForMask(), mMaskSnapshot(), mIsDisposed(false)
   {
+    printf("pxObjectTracking CREATION CONSTRUCTOR [%p] \n", this);
+    fflush(stdout);
     pxObjectCount++;
     mScene = scene;
     mReady = new rtPromise;
@@ -575,6 +578,8 @@ pxObject::~pxObject()
     mClipSnapshotRef = NULL;
     mDrawableSnapshotForMask = NULL;
     mMaskSnapshot = NULL;
+    printf("pxObjectTracking DESTRUCTION [%p] \n",this);
+    fflush(stdout);
 }
 
 void pxObject::sendPromise()
@@ -2048,6 +2053,8 @@ rtError pxScene2d::create(rtObjectRef p, rtObjectRef& o)
 
   if (needpxObjectTracking)
     mInnerpxObjects.push_back((pxObject*)o.getPtr());
+  printf("pxObjectTracking CREATION [%p] [%s] [%s] \n", o.getPtr(), t.cString(), mScriptView->getUrl().cString());
+  fflush(stdout);
   return e;
 }
 
