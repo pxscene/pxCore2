c99aec1263a9b3a9bc67aa4abd0aff8672422207
mthirumalai <madanagopal_thirumalai@comcast.com>
2018-05-23 08:36:43 +0530

    debug prints for pxobject tracking [TESTING PURPOSE ONLY]

--- examples/pxScene2d/src/pxScene2d.cpp
+++ examples/pxScene2d/src/pxScene2d.cpp
@@ -557,6 +557,8 @@ pxObject::pxObject(pxScene2d* scene): rtObject(), mParent(NULL), mcx(0), mcy(0),
 
 pxObject::~pxObject()
 {
+    printf("pxObjectTracking DESTRUCTION [%p] \n",this);
+    fflush(stdout);
 //    rtString d;
     // TODO... why is this bad
 //    sendReturns<rtString>("description",d);
@@ -2060,7 +2062,11 @@ rtError pxScene2d::create(rtObjectRef p, rtObjectRef& o)
   }
 
   if (needpxObjectTracking)
+  {
     mInnerpxObjects.push_back((pxObject*)o.getPtr());
+    printf("pxObjectTracking CREATION [%p] [%s] [%s] \n", o.getPtr(), t.cString(), mScriptView->getUrl().cString());
+    fflush(stdout);
+  }
   return e;
 }
 
