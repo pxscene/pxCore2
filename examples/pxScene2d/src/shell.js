3b2a5008755d1688b8440bada1aad4c98eaa2f02
FitzerIRL <Hugh_Fitzpatrick@hotmail.com>
2018-06-10 08:06:58 -0400

    Adding getColor() to create() for #rgb #rrggbb support.  Minor refactor of shell.js keys

--- examples/pxScene2d/src/shell.js
+++ examples/pxScene2d/src/shell.js
@@ -46,145 +46,94 @@ px.import({ scene: 'px:scene.1.js',
   var originalURL = (!url || url==="") ? "browser.js":url;
   console.log("url:",originalURL);
 
-  var    blackBg = scene.create({t:"rect", fillColor:0x000000ff,lineColor:0xffff0080,lineWidth:0,x:0,y:0,w:1280,h:720,a:0,parent:scene.root});
-  var childScene = scene.create({t:"scene", url:originalURL,parent:scene.root});
-  childScene.focus = true;
-
-  var showFPS = false;
-  var fpsBg      = scene.create({t:"rect", fillColor:0x00000080,lineColor:0xffff0080,lineWidth:3,x:10,y:10,a:showFPS?1:0,parent:scene.root});
-  var fpsCounter = scene.create({t:"text", x:5,textColor:0xffffffff,pixelSize:24,text:"0fps",parent:fpsBg});
-  fpsBg.w = fpsCounter.w+16;
-  fpsBg.h = fpsCounter.h;
-
-  // Prevent interaction with scenes...
-  fpsBg.interactive = false;
-  fpsCounter.interactive = false;
-
-  function updateSize(w, h) {
-    childScene.w = w;
-    childScene.h = h;
-    blackBg.w = w;
-    blackBg.h = h;
-  }
-
-  // TODO if I log out event object e... there is extra stuff??
-  scene.on("onResize", function(e) { updateSize(e.w, e.h);});
-  updateSize(scene.w, scene.h);
-
-  scene.on("onFPS", function(e) {
-    if(showFPS) {
-      fpsCounter.text = ""+Math.floor(e.fps)+"fps";
-      fpsBg.w = fpsCounter.w+16;
-      fpsBg.h = fpsCounter.h;
-    }
-  });
-
-////
-if (false)
-{
-  // TODO Cursor emulation mostly for egl targets right now.
-
-  // TODO hacky raspberry pi detection
-  var os = require("os");
-  var hostname = os.hostname();
-
-  if (hostname == "raspberrypi") {
-    var cursor = scene.create({t:"image", url:"cursor.png",parent:scene.root,
-                               interactive:false});
-
-    scene.on("onMouseMove", function(e) {
-	    cursor.x = e.x-23;
-	    cursor.y = e.y-10;
-    });
-  }
-}
-
-  scene.root.on("onPreKeyDown", function(e) {
-	  var code  = e.keyCode;
-    var flags = e.flags;
-
-    var loggingDisabled = process.env.PXSCENE_KEY_LOGGING_DISABLED;
-    if (loggingDisabled && loggingDisabled === '1'){
-      console.log("onPreKeyDown value hidden");
-    } else {
-      console.log("SHELL: onPreKeyDown:", code, " key: ", keys.name(code), ", ", flags);
-    }
-
-    if( keys.is_CTRL_ALT( flags ) )
-    {
-      if(code == keys.Y)  // ctrl-alt-y
+  var    blackBg = scene.create({t:"rect", fillColor:0x000000ff,x:0,y:0,w:1280,h:720,a:0,parent:scene.root});
+      switch(code)
       {
-//        console.log("SHELL: onPreKeyDown: FPS !!!  ############# ");
-
-        showFPS = !showFPS;
-        fpsBg.a = (showFPS)?1.0:0;
-        e.stopPropagation();
-      }
-      else
-      if(code == keys.O)  // ctrl-alt-o
-      {
-//        console.log("SHELL: onPreKeyDown: showOutlines !!!  ############# ");
-
-        scene.showOutlines = !scene.showOutlines;
-        e.stopPropagation();
-      }
-      else
-      if (code == keys.S)  // ctrl-alt-s
-      {
-        if (!isDuk) {
-        // This returns a data URI string with the image data
-        var dataURI = scene.screenshot('image/png;base64');
-
-        // convert the data URI by stripping off the scheme and type information
-        // to a base64 encoded string with just the PNG image data
-        var base64PNGData = dataURI.slice(dataURI.indexOf(',')+1);
-
-        // decode the base64 data and write it to a file
-        fs.writeFile("screenshot.png", new Buffer(base64PNGData, 'base64'), function(err)
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case keys.Y:  // ctrl-alt-y
         {
-          if (err)
-            console.log("Error creating screenshot.png");
-          else
-            console.log("Created screenshot.png");
-        });
-      }
-        e.stopPropagation();
-      }
-      else
-      if(code == keys.D)  // ctrl-alt-d
-      {
-        // console.log("SHELL: onPreKeyDown: show dirty rect !!!  ############# ");
+          showFPS = !showFPS;
+          fpsBg.a = (showFPS)?1.0:0;
+          e.stopPropagation();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case keys.O:  // ctrl-alt-o
+        {
+          scene.showOutlines = !scene.showOutlines;
+          e.stopPropagation();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case keys.S:  // ctrl-alt-s
+        {
+          if (!isDuk)
+          {
+            // This returns a data URI string with the image data
+            var dataURI = scene.screenshot('image/png;base64');
 
-        scene.showDirtyRect = !scene.showDirtyRect;
-        e.stopPropagation();
-      }
+            // convert the data URI by stripping off the scheme and type information
+            // to a base64 encoded string with just the PNG image data
+            var base64PNGData = dataURI.slice(dataURI.indexOf(',')+1);
+
+            // decode the base64 data and write it to a file
+            fs.writeFile("screenshot.png", new Buffer(base64PNGData, 'base64'), function(err)
+            {
+              if (err)
+                console.log("Error creating screenshot.png");
+              else
+                console.log("Created screenshot.png");
+            });
+          }
+          e.stopPropagation();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case  keys.D:  // ctrl-alt-d
+        {
+          // console.log("SHELL: onPreKeyDown: show dirty rect !!!  ############# ");
+
+          scene.showDirtyRect = !scene.showDirtyRect;
+          e.stopPropagation();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      }//SWITCH ctrl-alt
     }// ctrl-alt
     else
     if( keys.is_CTRL_ALT_SHIFT( flags ) )
     {
-      if(code == keys.R)  // ctrl-alt-shft-r
+      switch(code)
       {
-        // console.log("SHELL: onPreKeyDown: Reloading url [ "+originalURL+" ] !!!  ############# ");
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case keys.R:  // ctrl-alt-shft-r
+        {
+          // console.log("SHELL: onPreKeyDown: Reloading url [ "+originalURL+" ] !!!  ############# ");
 
-        console.log("Reloading url: ", originalURL);
-        childScene.url = originalURL;
-        e.stopPropagation();
-      }
-      else
-      if(code == keys.H)  // ctrl-alt-shft-h
-      {
-        // console.log("SHELL: onPreKeyDown: Loading HOME url [ "+"browser.js"+" ] !!!  ############# ");
+          console.log("Reloading url: ", originalURL);
+          childScene.url = originalURL;
+          e.stopPropagation();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case keys.H:  // ctrl-alt-shft-h
+        {
+          // console.log("SHELL: onPreKeyDown: Loading HOME url [ "+"browser.js"+" ] !!!  ############# ");
 
-        var homeURL = "browser.js";
-        console.log("Loading home url: ", homeURL);
-        childScene.url = homeURL;
-        e.stopPropagation();
-      }
-      else
-      if(code == keys.D)  // ctrl-alt-shft-d
-      {
-	scene.logDebugMetrics();
-      }
+          var homeURL = "browser.js";
+          console.log("Loading home url: ", homeURL);
+          childScene.url = homeURL;
+          e.stopPropagation();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        case keys.D:  // ctrl-alt-shft-d
+        {
+          scene.logDebugMetrics();
+        }
+        break;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      }//SWITCH
     }// ctrl-alt-shift
   });
 
@@ -228,20 +177,27 @@ if (false)
 
       if( keys.is_CTRL_ALT( flags ) )
       {
-        if(code == keys.R)   // ctrl-alt-r
+         switch(code)
         {
-          console.log("(shell.js) Reloading url: ", originalURL);
-          childScene.url = originalURL;
-          e.stopPropagation();
-        }
-        else
-        if (code == keys.H)  // ctrl-alt-h
-        {
-          var homeURL = "browser.js";
-          console.log("Loading home url: ", homeURL);
-          childScene.url = homeURL;
-          e.stopPropagation();
-        }
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            case keys.R: // ctrl-alt-r
+            {
+              console.log("(shell.js) Reloading url: ", originalURL);
+              childScene.url = originalURL;
+              e.stopPropagation();
+            }
+            break;
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            case keys.H: // ctrl-alt-h
+            {
+              var homeURL = "browser.js";
+              console.log("Loading home url: ", homeURL);
+              childScene.url = homeURL;
+              e.stopPropagation();
+            }
+            break
+            // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        }//SWITCH
       }// ctrl-alt
     });
   }
@@ -278,7 +234,6 @@ if (false)
       {
         if(childScene.api.wantsClearscreen()) // use delegate preference - returns bool
           blackBg.a = 1; 
-
       }
       else {
           blackBg.a = 1; 
@@ -289,12 +244,13 @@ if (false)
   //
   // Returns:   BOOL preference of 'shell' performing Clearscreen per frame.
   /*
-  module.exports.wantsClearscreen1 = function()  // delegate
+  module.exports.wantsClearscreen = function()  // delegate
   {
     return false;
   };
   */
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   function releaseResources() {
     if (!isDuk) {
       process.removeListener("uncaughtException", uncaughtException);
