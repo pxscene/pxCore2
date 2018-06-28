pxscene/pxCore2

commit a969b5681359ab4de04376322dbd646466d4219d

Possible fix for deleting event listeners from javascript (#1041)
* Fix for deleting event listeners from javascript

* Fix for deleting event listeners from javascript

src/rtObject.cpp
@@ -70,7 +70,8 @@ rtError rtEmit::addListener(const char* eventName, rtIFunction* f)
        it != mEntries.end(); it++)
   {
     _rtEmitEntry& e = (*it);
-    if (e.n == eventName && e.f.getPtr() == f && !e.isProp)
+    // mHash check for javscript events callback 
+    if (e.n == eventName && ((e.f.getPtr() == f) || ((f->hash() != -1) && (e.fnHash == f->hash()))) && !e.isProp)
     {
       found = true;
       break;
@@ -82,6 +83,8 @@ rtError rtEmit::addListener(const char* eventName, rtIFunction* f)
     e.n = eventName;
     e.f = f;
     e.isProp = false;
+    e.markForDelete = false;
+    e.fnHash = f->hash();
     mEntries.push_back(e);
   }
   
@@ -90,18 +93,24 @@ rtError rtEmit::addListener(const char* eventName, rtIFunction* f)
 
 rtError rtEmit::delListener(const char* eventName, rtIFunction* f)
 {
+  if (!eventName || !f)
+    return RT_ERROR;
+  int i = 0;
   for (vector<_rtEmitEntry>::iterator it = mEntries.begin(); 
        it != mEntries.end(); it++)
   {
     _rtEmitEntry& e = (*it);
-    if (e.n == eventName && e.f.getPtr() == f && !e.isProp)
+    if (e.n == eventName && ((e.f.getPtr() == f) || ((-1 != e.fnHash) && (e.fnHash == f->hash()))) && !e.isProp)
     {
-      mEntries.erase(it);
+      // if no events is being processed currently, remove the event entries
+      if (!mProcessingEvents)
+      	mEntries.erase(it);
+      else
+        it->markForDelete = true;
       // There can only be one
       break;
     }
   }
-  
   return RT_OK;
 }
 
@@ -114,6 +123,8 @@ rtError rtEmit::Send(int numArgs, const rtValue* args, rtValue* result)
     rtLogDebug("rtEmit::Send %s", eventName.cString());
 
     vector<_rtEmitEntry>::iterator it = mEntries.begin();
+    
+    mProcessingEvents = true;
     while (it != mEntries.end())
     {
       _rtEmitEntry& e = (*it);
@@ -147,6 +158,19 @@ rtError rtEmit::Send(int numArgs, const rtValue* args, rtValue* result)
         {
           ++it;
         }
+      }
+      else
+      {
+        ++it;
+      }
+    }
+    mProcessingEvents = false;
+    it = mEntries.begin();
+    while (it != mEntries.end())
+    {
+      if (true == it->markForDelete)
+      {
+        it = mEntries.erase(it);
       }
       else
       {

src/rtObject.h
@@ -60,6 +60,8 @@ class rtIFunction
     virtual unsigned long AddRef()=0;
     virtual unsigned long Release()=0;
     virtual rtError Send(int numArgs, const rtValue* args, rtValue* result) = 0;
+    virtual size_t hash() = 0;
+    virtual void setHash(size_t) = 0;
 };
 
 class rtObjectRef;
@@ -273,6 +275,16 @@ public:
     long l = rtAtomicDec(&mRefCount);
     if (l == 0) delete this;
     return l;
+  }
+
+  virtual size_t hash()
+  {
+    return -1;
+  }
+
+  virtual void setHash(size_t hash)
+  {
+    UNUSED_PARAM(hash);
   }
 
  private:
@@ -605,6 +617,16 @@ public:
     return mCB(numArgs, args, result, mContext);
   }
   
+  virtual size_t hash()
+  {
+    return -1;
+  }
+
+  virtual void setHash(size_t hash)
+  {
+    UNUSED_PARAM(hash);
+  }
+
   void clearContext()
   {
     mContext = NULL;
@@ -623,7 +645,7 @@ class rtEmit: public rtIFunction
 {
 
 public:
-  rtEmit(): mRefCount(0) {}
+  rtEmit(): mRefCount(0), mProcessingEvents(false) {}
   virtual ~rtEmit() {}
 
   virtual unsigned long AddRef();
@@ -637,16 +659,29 @@ public:
 
   virtual rtError Send(int numArgs,const rtValue* args,rtValue* result);
 
+  virtual size_t hash()
+  {
+    return -1;
+  }
+
+  virtual void setHash(size_t hash)
+  {
+    UNUSED_PARAM(hash);
+  }
+
 protected:
   struct _rtEmitEntry 
   {
     rtString n;
     rtFunctionRef f;
     bool isProp;
+    bool markForDelete;
+    size_t fnHash;
   };
   
   std::vector<_rtEmitEntry> mEntries;
   rtAtomic mRefCount;
+  bool mProcessingEvents;
 };
 
 class rtEmitRef: public rtRef<rtEmit>, public rtFunctionBase

src/rtScriptDuk/rtFunctionWrapperDuk.h
@@ -11,7 +11,6 @@ extern "C" {
 
 namespace rtScriptDukUtils
 {
-
 class rtFunctionWrapper : public rtWrapper<rtFunctionRef, rtFunctionWrapper>
 {
 public:
@@ -25,13 +24,21 @@ public:
 class jsFunctionWrapper : public rtIFunction
 {
 public:
-  jsFunctionWrapper(duk_context *ctx, const std::string &funcName) : mRefCount(0), mDukCtx(ctx), mDukFuncName(funcName), mComplete(false), mTeardownThreadingPrimitives(false) {}
+  jsFunctionWrapper(duk_context *ctx, const std::string &funcName) : mRefCount(0), mDukCtx(ctx), mDukFuncName(funcName), mComplete(false), mTeardownThreadingPrimitives(false), mHash(-1) { }
   virtual ~jsFunctionWrapper();
 
   virtual unsigned long AddRef();
   virtual unsigned long Release();
   virtual unsigned long getRefCount() const {
     return mRefCount;
+  }
+  virtual size_t hash() {
+    return mHash;
+  }
+
+  virtual void setHash(size_t hash)
+  {
+    mHash = hash;
   }
 #if 0
   void signal(rtValue const& returnValue);
@@ -64,7 +71,7 @@ private:
   pthread_cond_t mCond;
 #endif
 #endif
-
+  size_t mHash;
   rtValue mReturnValue;
 };
 

src/rtScriptDuk/rtWrapperUtilsDuk.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 namespace rtScriptDukUtils
 {
+std::hash<std::string> hashFn;
 
 void rt2duk(duk_context *ctx, const rtValue& v)
 {
@@ -120,7 +121,17 @@ rtValue duk2rt(duk_context *ctx, rtWrapperError* error)
   if (duk_is_function(ctx, -1)) {
     duk_dup(ctx, -1);
     std::string id = rtDukPutIdentToGlobal(ctx);
-    return rtValue(rtFunctionRef(new jsFunctionWrapper(ctx, id)));
+    jsFunctionWrapper *wr = new jsFunctionWrapper(ctx, id);
+    duk_dump_function(ctx);
+    unsigned  char *p;
+    duk_size_t sz;
+    p = (unsigned char *) duk_get_buffer(ctx, -1, &sz);
+    if (NULL != p)
+    {
+      size_t fnval = hashFn((char *)p);
+      wr->setHash(fnval);
+    }
+    return rtValue(rtFunctionRef(wr));
   }
   if (duk_is_object(ctx, -1)) {
     duk_bool_t res = duk_get_prop_string(ctx, -1, "\xff""\xff""data");

src/rtScriptNode/rtFunctionWrapper.cpp
@@ -11,6 +11,7 @@ namespace rtScriptNodeUtils
 
 static const char* kClassName = "Function";
 static Persistent<v8::Function> ctor;
+std::hash<std::string> hashFn;
 
 static void jsFunctionCompletionHandler(void* argp, rtValue const& result)
 {
@@ -267,7 +268,12 @@ jsFunctionWrapper::jsFunctionWrapper(Local<Context>& ctx, const Handle<Value>& v
   , mFunction(ctx->GetIsolate(), Handle<Function>::Cast(val))
   , mComplete(false)
   , mTeardownThreadingPrimitives(false)
+  , mHash(-1)
 {
+  v8::String::Utf8Value fn(Handle<Function>::Cast(val)->ToString());
+  if (NULL != *fn) { 
+    mHash = hashFn(*fn);
+  }
   mIsolate = ctx->GetIsolate();
   mContext.Reset(ctx->GetIsolate(), ctx);
   assert(val->IsFunction());
@@ -285,6 +291,7 @@ jsFunctionWrapper::~jsFunctionWrapper()
     pthread_cond_destroy(&mCond);
 #endif
   }
+  mHash = -1;
 }
 
 void jsFunctionWrapper::setupSynchronousWait()

src/rtScriptNode/rtFunctionWrapper.h
@@ -67,7 +67,14 @@ public:
   virtual unsigned long getRefCount() const {
     return mRefCount;
   }
+  virtual size_t hash() {
+    return mHash;
+  }
 
+  virtual void setHash(size_t hash) {
+    UNUSED_PARAM(hash);
+  }
+ 
   void signal(rtValue const& returnValue);
 
 private:
@@ -104,7 +111,7 @@ private:
   pthread_mutex_t mMutex;
   pthread_cond_t mCond;
 #endif
-
+  size_t mHash;
   rtValue mReturnValue;
 };
 
@@ -120,6 +127,15 @@ public:
   rtResolverFunction(Disposition d, v8::Local<v8::Context>& ctx, v8::Local<v8::Promise::Resolver>& resolver);
   virtual ~rtResolverFunction();
   virtual rtError Send(int numArgs, const rtValue* args, rtValue* result);
+  virtual size_t hash()
+  {
+    return -1;
+  }
+
+  virtual void setHash(size_t hash) {
+    UNUSED_PARAM(hash);
+  }
+
 
 private:
   struct AsyncContext

tests/pxScene2d/CMakeLists.txt
@@ -126,7 +126,7 @@ endif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
 
 add_definitions(-D${PX_PLATFORM} -DENABLE_RT_NODE -DRUNINMAIN -DENABLE_HTTP_CACHE)
 
-set(TEST_SOURCE_FILES pxscene2dtestsmain.cpp  test_example.cpp test_api.cpp  test_pxcontext.cpp test_memoryleak.cpp test_rtnode.cpp test_rtMutex.cpp test_pxImage9Border.cpp
+set(TEST_SOURCE_FILES pxscene2dtestsmain.cpp  test_example.cpp test_api.cpp  test_pxcontext.cpp test_memoryleak.cpp test_rtnode.cpp test_rtMutex.cpp test_pxImage9Border.cpp test_eventListeners.cpp
     test_pxAnimate.cpp test_rtFile.cpp test_rtZip.cpp test_rtString.cpp test_rtValue.cpp test_pxImage.cpp test_pxOffscreen.cpp test_pxMatrix4T.cpp test_rtObject.cpp
     test_pxWindowUtil.cpp test_pxTexture.cpp test_pxWindow.cpp test_ioapi.cpp test_rtLog.cpp test_pxTimerNative.cpp
     test_rtUrlUtils.cpp test_pxArchive.cpp test_pxPixel_h.cpp test_pxFont.cpp test_rtThreadPool.cpp test_utf8.cpp ${PLATFORM_TEST_FILES} ${TEST_WAYLAND_SOURCE_FILES})

tests/pxScene2d/eventListeners.js
@@ -1,0 +1,25 @@
+px.import({
+    scene: 'px:scene.1.js'
+  })
+  .then(function importsReady(imports) {
+    var scene = imports.scene;
+    var root = scene.root;
+
+    let rect_one = scene.create({
+      parent: scene.root,
+      t: 'rect',
+      id: 'one',
+      x: 50,
+      y: 50,
+      w: 100,
+      h: 100
+    });
+
+    function listener(event) {
+      console.log("Event received");
+    }
+
+    scene.on("addEventsProper", function() { rect_one.on('onListener', listener); console.log("Received add events proper **************");  });
+    scene.on("removeEventsProper", function() { rect_one.delListener("onListener", listener); });
+    scene.on("removeEventsImProper", function() { rect_one.delListener("onListener", null); });
+  });

commit 9ce7dc283f0bb389b2edffbf2e6fe57368cddbe3

Fix _rtEmitEntry intilization bug
Fix bug caused by commit:
a969b568

All members of the entry were not being initialized when
calling setListener causing undefined behaviour like all
entries being deleted after the first call to emit send.

src/rtObject.cpp
@@ -54,6 +54,8 @@ rtError rtEmit::setListener(const char* eventName, rtIFunction* f)
     e.n = eventName;
     e.f = f;
     e.isProp = true;
+    e.markForDelete = false;
+    e.fnHash = f->hash();
     mEntries.push_back(e);      
   }
   