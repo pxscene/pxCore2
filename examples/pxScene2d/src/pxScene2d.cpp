2be2bc9d719a6902b635b7514914e4122d28a051
madanagopalt <madanagopal123@gmail.com>
2018-07-14 08:29:02 +0530

    Update pxScene2d.cpp

--- examples/pxScene2d/src/pxScene2d.cpp
+++ examples/pxScene2d/src/pxScene2d.cpp
@@ -1941,6 +1941,7 @@ rtError pxScene2d::dispose()
 {
     mDisposed = true;
     rtObjectRef e = new rtMapObject;
+    // pass false to make onClose asynchronous
     mEmit.send("onClose", false, e);
     for (unsigned int i=0; i<mInnerpxObjects.size(); i++)
     {
@@ -1959,6 +1960,7 @@ rtError pxScene2d::dispose()
     #endif //ENABLE_RT_NODE
     // send scene terminate after dispose to make sure, no cleanup can happen further on app side
     // after clearing the sandbox
+    // pass false to make onSceneTerminate asynchronous
     mEmit.send("onSceneTerminate", false, e);
     mEmit->clearListeners();
 
