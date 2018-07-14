7860cd61c432b2abe6c3c98eb8ed32bc26a6ddfa
madanagopalt <madanagopal123@gmail.com>
2018-07-14 14:51:53 +0530

    Update AppSceneContext.js

--- examples/pxScene2d/src/rcvrcore/AppSceneContext.js
+++ examples/pxScene2d/src/rcvrcore/AppSceneContext.js
@@ -110,6 +110,7 @@ if( fullPath !== null)
   this.loadPackage(fullPath);
 
 this.innerscene.on('onSceneTerminate', function (e) {
+    console.log("Scene terminate called" + this.packageUrl);
     if (null != this.webSocketManager)
     {
        this.webSocketManager.clearConnections();
