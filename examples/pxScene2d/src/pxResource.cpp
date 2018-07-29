0c0bf819670f9be39eb8f4401a23e1ad46d98b86
madanagopalt <madanagopal123@gmail.com>
2018-07-29 10:02:26 +0530

    Update pxResource.cpp

--- examples/pxScene2d/src/pxResource.cpp
+++ examples/pxScene2d/src/pxResource.cpp
@@ -504,8 +504,8 @@ void pxResource::loadResource()
   if (mUrl.beginsWith("http:") || mUrl.beginsWith("https:"))
   {
       setLoadStatus("sourceType", "http");
-      printf("pxResource::loadResource download initiated [%s] \n",mUrl.cString());
-      fflush(stdout); 
+      printf("pxResource::loadResource initiated [%s] \n",mUrl.cString());
+      fflush(stdout);
       mDownloadRequest = new rtFileDownloadRequest(mUrl, this, pxResource::onDownloadComplete);
       mDownloadRequest->setProxy(mProxy);
       mDownloadRequest->setCallbackFunctionThreadSafe(pxResource::onDownloadComplete);
@@ -654,8 +654,6 @@ uint32_t rtImageResource::loadResourceData(rtFileDownloadRequest* fileDownloadRe
 void pxResource::processDownloadedResource(rtFileDownloadRequest* fileDownloadRequest)
 {
   rtString val = "reject";
-  printf("pxResource::loadResource processDownloadedResource [%s] \n",mUrl.cString());
-  fflush(stdout); 
   if (fileDownloadRequest != NULL)
   {
     bool wasCanceled = fileDownloadRequest->isCanceled();
