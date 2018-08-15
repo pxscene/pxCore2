f7eee25e92f772a6b5db6227abe7204ad7954722
arun-govindan <30860944+arun-govindan@users.noreply.github.com>
2018-08-15 17:21:50 -0400

    Updated variable name

--- examples/pxScene2d/src/mkdeploy.sh
+++ examples/pxScene2d/src/mkdeploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-appName=Spark
+APPNAME=Spark
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]
 then
-appName=SparkEdge
+APPNAME=SparkEdge
 else
-appName=Spark
+APPNAME=Spark
 fi
 
 function mkUpdate() {
 
 INFILE=macstuff/software_update.plist
 OUTFILE=deploy/mac/software_update.plist
-DMGFILE=deploy/mac/${appName}.dmg
+DMGFILE=deploy/mac/${APPNAME}.dmg
 
-SIZE=`stat -f "%z" deploy/mac/${appName}.dmg`
+SIZE=`stat -f "%z" deploy/mac/${APPNAME}.dmg`
 HASH=`openssl sha1 -binary "${DMGFILE}" | openssl base64`
 VERS=$1
 
@@ -28,9 +28,9 @@ m4 -D__HASH__="${HASH}" -D__SIZE__="${SIZE}" -D__VERSION__="${VERS}" ${INFILE} >
 DEPLOY_DIR=deploy/mac/.stage
 PX_SCENE_VERSION=$1
 mkdir -p $DEPLOY_DIR
-rm -r $DEPLOY_DIR/${appName}.app
-cp -a ${appName}.app $DEPLOY_DIR
-echo $PX_SCENE_VERSION > $DEPLOY_DIR/${appName}.app/Contents/MacOS/version
+rm -r $DEPLOY_DIR/${APPNAME}.app
+cp -a ${APPNAME}.app $DEPLOY_DIR
+echo $PX_SCENE_VERSION > $DEPLOY_DIR/${APPNAME}.app/Contents/MacOS/version
 
 #build dmg
 ./mkdmg.sh
