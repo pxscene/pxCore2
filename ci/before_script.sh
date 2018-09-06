cbb3863f67e896fc4915aa754f836fc61060e880
Connie Fry <conniefry@users.noreply.github.com>
2018-09-06 14:00:54 -0700

    Use testRunner that can take multiple tests files (#1430)
    * split tests.json into multiple files for stb testing
    
    * use relative path to test
    
    * remove typo
    
    * split tests.json into multiple files for stb testing
    
    * use relative path to test
    
    * remove typo
    
    * split tests.json into multiple files for stb testing
    
    * use relative path to test
    
    * remove typo
    
    * split tests.json into multiple files for stb testing
    
    * remove typo
    
    * rename tests file for clarity
    
    * Revert "rename tests file for clarity"
    
    This reverts commit 109b2d759680dae122061dfa33abb2b61826fd91.
    
    * rename tests file for clarity
    
    * change gallery3 name to remove duplicate from testsDesktop.json; update info on testRunner

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 TESTS_DIR="${TRAVIS_BUILD_DIR}/tests/pxScene2d"
+TESTSDESKTOP_JSON="${TESTS_DIR}/testRunner/testsDesktop.json"
+cp "${TESTSDESKTOP_JSON}" "${TESTS_DIR}/testRunner/testsDesktop.json_orig"
+
 TESTS_JSON="${TESTS_DIR}/testRunner/tests.json"
 cp "${TESTS_JSON}" "${TESTS_DIR}/testRunner/tests.json_orig"
 
@@ -18,7 +21,7 @@ drop_json_lines()
 }
 
 #make arrangements for ignoring some tests
-drop_json_lines "pxWayland" "${TESTS_JSON}"
+drop_json_lines "pxWayland" "${TESTSDESKTOP_JSON}"
 if ! grep -q "TEST_PERMISSIONS_CHECK\" ON" "${TESTS_DIR}/CMakeLists.txt" ; then
   drop_json_lines "permissions" "${TESTS_JSON}"
 fi
