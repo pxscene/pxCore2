102596dbb82f7303c489d2290b7f10bdc30c87c6
arun-govindan <30860944+arun-govindan@users.noreply.github.com>
2018-09-06 09:58:58 -0400

    To avoid release builds getting downloaded in place of edge (#1419)
    * To avoid release builds getting downloaded in place of edge
    
    * Updated artifact name
    
    * Update uploadWindowsArtifact.sh

--- ci/uploadWindowsArtifact.sh
+++ ci/uploadWindowsArtifact.sh
@@ -3,18 +3,24 @@ counter=0
 jobNum=0
 jobNum=`echo "$(curl -sS --header "Content-type: application/json" "https://ci.appveyor.com//api/projects/pxscene/pxcore" | jq -r ".build.buildNumber")"` 
 jobId=0dev.$jobNum
-while [  $counter -lt 30 ]; do
+while [  $counter -lt 20 ]; do
   buildList=".builds[$counter].version"
   jobId=0dev.$jobNum
   let counter=counter+1
   let jobNum=jobNum-1 
   if [ "$jobId" != null ] ; then
     build="$(curl -sS --header "Content-type: application/json" "https://ci.appveyor.com/api/projects/pxscene/pxcore/build/"$jobId)" 
+    if [[ "${build}" =  *"Build not found"* ]] ; then
+      continue
+    fi
     artifactStr=".build.jobs[0].artifactsCount"
-    buildStr=".build.jobs[0].jobId" 
+    buildStr=".build.jobs[0].jobId"
+    tagValStr=".build.isTag"
     artifactCounts=$(echo $build | jq -r  $artifactStr)
     buildVer=$(echo $build | jq -r  $buildStr)
-    if [ $artifactCounts -gt 0 ] ; then
+    isTagTrue=$(echo $build | jq -r $tagValStr)
+
+    if [[ $artifactCounts -gt 0 ]] && [[ "$isTagTrue" = "false" ]] ; then
       downloadArtifact="wget -q https://ci.appveyor.com/api/buildjobs/"$buildVer"/artifacts/spark-setup.exe"
       echo "Artifact count : $artifactCounts, Build version :  $buildVer, JobId : $jobId)"
       $downloadArtifact
