dwrobel/pxCore

local access file - 2nd generation
Allow NODE_PATH environment variable to contain multiple directories
separated by:
  - ';' on windows platform,
  - ':' on any other.

Notes:
  - The first entry in NODE_PATH is used to load SANDBOX_JS file
    (this preserves backward compatibility).

  - If NODE_PATH is not defined then the default value is obtained
    from getcwd(3).

examples/pxScene2d/src/pxResource.cpp
@@ -27,6 +27,7 @@
 #include "pxResource.h"
 #include "pxUtil.h"
 #include "rtThreadPool.h"
+#include "rtPathUtils.h"
 
 using namespace std;
 
@@ -556,6 +557,27 @@ void rtImageResource::loadResourceFromFile()
   rtString status = "resolve";
   rtData d;
   rtError loadImageSuccess = rtLoadFile(mUrl, d);
+
+  do
+  {
+    if (loadImageSuccess == RT_OK)
+      break;
+
+    if (rtIsPathAbsolute(mUrl))
+      break;
+
+    rtModuleDirs *dirs = rtModuleDirs::instance();
+
+    for (rtModuleDirs::iter it = dirs->iterator(); it.first != it.second; it.first++)
+    {
+      if (rtLoadFile(rtConcatenatePath(*it.first, mUrl.cString()).c_str(), d) == RT_OK)
+      {
+        loadImageSuccess = RT_OK;
+        break;
+      }
+    }
+  } while(0);
+
   if (loadImageSuccess == RT_OK)
   {
     loadImageSuccess = pxLoadImage((const char *) d.data(), d.length(), imageOffscreen, init_w, init_h, init_sx, init_sy);

pxscene/pxCore2

examples/pxScene2d/src/pxResource.cpp
@@ -27,6 +27,7 @@
 #include "pxResource.h"
 #include "pxUtil.h"
 #include "rtThreadPool.h"
+#include "rtPathUtils.h"
 
 
 using namespace std;
@@ -574,15 +575,33 @@ void rtImageResource::loadResourceFromFile()
 
   rtError loadImageSuccess = RT_FAIL;
 
-  if(mData.length() == 0)
+  do
   {
+    if (mData.length() != 0)
+    {
+      // We have BASE64 or SVG string already...
+      loadImageSuccess = RT_OK;
+      break;
+    }
+
     loadImageSuccess = rtLoadFile(mUrl, mData);
-  }
-  else
-  {
-    // We have BASE64 or SVG string already...
-    loadImageSuccess = RT_OK;
-  }
+    if (loadImageSuccess == RT_OK)
+      break;
+
+    if (rtIsPathAbsolute(mUrl))
+      break;
+
+    rtModuleDirs *dirs = rtModuleDirs::instance();
+
+    for (rtModuleDirs::iter it = dirs->iterator(); it.first != it.second; it.first++)
+    {
+      if (rtLoadFile(rtConcatenatePath(*it.first, mUrl.cString()).c_str(), mData) == RT_OK)
+      {
+        loadImageSuccess = RT_OK;
+        break;
+      }
+    }
+  } while(0);
 
   if (loadImageSuccess == RT_OK)
   {