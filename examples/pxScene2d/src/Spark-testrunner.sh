09c7c1b69600b969b57e9bd3eae1758d26e7eb22
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2018-09-07 14:40:46 +0200

    add exit test to testRunner (#1180)

--- examples/pxScene2d/src/Spark-testrunner.sh
+++ examples/pxScene2d/src/Spark-testrunner.sh
@@ -19,8 +19,8 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 pushd $SCRIPT_DIR
 
 SPARK_BIN="${SPARK_BIN:-./Spark}"
-SPARK_URL="${SPARK_URL:-https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner_v5.2.js?tests=}"
-SPARK_TEST="${SPARK_TEST:-file:../../../tests/pxScene2d/testRunner/tests.json}"
+SPARK_URL="${SPARK_URL:-https://px-apps.sys.comcast.net/pxscene-samples/examples/px-reference/test-run/testRunner_v7.js?tests=}"
+SPARK_TEST="${SPARK_TEST:-file:../../../tests/pxScene2d/testRunner/testsDesktop.json,file:../../../tests/pxScene2d/testRunner/tests.json,file:../../../tests/pxScene2d/testRunner/testsExit.json}"
 
 ${DBG} ${SPARK_BIN} "${SPARK_URL}${SPARK_TEST}"
 
