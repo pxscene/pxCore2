915cc953212f0e4400a6bd8feb9f4acd0d8474eb
AshwiniKalokhe <ashwini_kalokhe@example.com>
2018-10-02 15:50:06 -0400

    revert

--- examples/pxScene2d/src/pxScene2d.h
+++ examples/pxScene2d/src/pxScene2d.h
@@ -509,9 +509,10 @@ public:
       // translate based on xy rotate/scale based on cx, cy
       bool doTransToOrig = msx != 1.0 || msy != 1.0 || mr;
       if (doTransToOrig)
-          m.translate(mx + mcx + dx, my + mcy + dy);
+        m.translate(mx + mcx + dx, my + mcy + dy);
       else
-          m.translate(mx + dx, my + dy);
+        m.translate(mx + dx, my + dy);
+        
       if (mr)
       {
         m.rotateInDegrees(mr
@@ -521,8 +522,7 @@ public:
         );
       }
       if (msx != 1.0 || msy != 1.0) m.scale(msx, msy);
-      if (doTransToOrig)
-          m.translate(-mcx, -mcy);
+      m.translate(-mcx, -mcy);
 #else
       // translate/rotate/scale based on cx, cy
       m.translate(mx, my);
@@ -534,7 +534,8 @@ public:
         );
       }
       if (msx != 1.0 || msy != 1.0) m.scale(msx, msy);
-      m.translate(-mcx, -mcy);
+      if (doTransToOrig)
+        m.translate(-mcx, -mcy);
 #endif
     }
     else
