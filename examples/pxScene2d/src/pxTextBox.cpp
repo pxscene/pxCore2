4ba53e0d92e6ea07eaff3505a5738334ca31a5c3
AshwiniKalokhe <ashwini_kalokhe@example.com>
2018-10-18 12:57:18 -0400

    [pxTextBox] Improve multibyte character handling

--- examples/pxScene2d/src/pxTextBox.cpp
+++ examples/pxScene2d/src/pxTextBox.cpp
@@ -388,25 +388,14 @@ void pxTextBox::measureTextWithWrapOrNewLine(const char *text, float sx, float s
     int i = 0;
     int lasti = 0;
     int numbytes = 1;
-    char* tempChar = NULL;
+    //char* tempChar = NULL;
     while((charToMeasure = u8_nextchar((char*)text, &i)) != 0)
     {
       // Determine if the character is multibyte
       numbytes = i-lasti;
-      if (tempChar != NULL)
-      {
-        delete [] tempChar;
-      }
-      tempChar = new char[numbytes+1];
-      memset(tempChar, '\0', sizeof(char)*(numbytes+1));
-      if(numbytes == 1) {
-        tempChar[0] = charToMeasure;
-      } 
-      else {
-        for( int pos = 0; pos < numbytes ; pos++) {
-          tempChar[pos] = text[lasti+pos];
-        }
-      }
+        
+      std::string tempChar = std::string (&text[lasti], numbytes);
+        
       lasti = i;
 
       if (getFontResource() != NULL)
@@ -430,7 +419,7 @@ void pxTextBox::measureTextWithWrapOrNewLine(const char *text, float sx, float s
       // Check if text still fits on this line, or if wrap needs to occur
       if( (tempX + charW) <= lineWidth || (!mWordWrap && lineNumber == 0))
       {
-        accString.append(tempChar);
+        accString.append(tempChar.c_str());
         tempX += charW;
       }
       else
@@ -469,7 +458,7 @@ void pxTextBox::measureTextWithWrapOrNewLine(const char *text, float sx, float s
               }
               else
               {
-                accString.append(tempChar);
+                accString.append(tempChar.c_str());
                 tempX += charW;
                 continue;
               }
@@ -522,13 +511,13 @@ void pxTextBox::measureTextWithWrapOrNewLine(const char *text, float sx, float s
             {
               //rtLogDebug("space char check to add to string: \"%s\"\n",accString.cString());
               //rtLogDebug("space char check: \"%s\"\n",tempChar);
-              accString.append(tempChar);
+              accString.append(tempChar.c_str());
             }
             
           }
 
-          delete [] tempChar;
-          tempChar = NULL;
+          /*delete [] tempChar;
+          tempChar = NULL;*/
 
           // Free tempStr
           free(tempStr);
@@ -557,11 +546,11 @@ void pxTextBox::measureTextWithWrapOrNewLine(const char *text, float sx, float s
         }
       }
     }//WHILE
-    if (tempChar != NULL)
+    /*if (tempChar != NULL)
     {
       delete [] tempChar;
       tempChar = NULL;
-    }
+    }*/
 
     if(accString.length() > 0) {
       lastLineNumber = lineNumber;
