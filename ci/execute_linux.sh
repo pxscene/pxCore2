1d3786ea103f871389e9ddb6bf91295afa399c7c
madanagopaltcomcast <35588028+madanagopaltcomcast@users.noreply.github.com>
2018-12-04 14:02:43 +0530

    Update execute_linux.sh

--- ci/execute_linux.sh
+++ ci/execute_linux.sh
@@ -56,6 +56,7 @@ printExecLogs()
 printValgrindLogs()
 {
   grep -A 200 "definitely lost" $VALGRINDLOGS
+  
   printf "\n********************** PRINTING VALGRIND LOG **************************\n"
   tail -150 $VALGRINDLOGS
   printf "\n**********************     LOG ENDS      **************************\n"
