fdf767674f91b658f7a2311fa63538e221a097d0
npoltorapavlo <npoltorapavlo@productengine.com>
2018-12-04 21:01:24 +0200

    fixes for v8: (#1573)
    * fixes for v8:
    - normal string formatting for console module
    - a crash when parsing empty headers
    - extra methods (once, removeAllListeners) for http classes
    
    * for v8 copied querystring module from libnode-v6.9.0 as is
    
    * add missing license header

--- src/rtHttpResponse.h
+++ src/rtHttpResponse.h
@@ -34,6 +34,9 @@ public:
   rtReadOnlyProperty(headers, headers, rtObjectRef);
 
   rtMethod2ArgAndNoReturn("on", addListener, rtString, rtFunctionRef);
+  rtMethod2ArgAndNoReturn("once", once, rtString, rtFunctionRef);
+  rtMethodNoArgAndNoReturn("removeAllListeners", removeAllListeners);
+  rtMethod1ArgAndNoReturn("removeAllListeners", removeAllListenersByName, rtString);
 
   rtHttpResponse();
   ~rtHttpResponse();
@@ -41,7 +44,10 @@ public:
   rtError statusCode(int32_t& v) const;
   rtError errorMessage(rtString& v) const;
   rtError headers(rtObjectRef& v) const;
-  rtError addListener(rtString eventName, const rtFunctionRef& f);
+  rtError addListener(const rtString& eventName, const rtFunctionRef& f);
+  rtError once(const rtString& eventName, const rtFunctionRef& f);
+  rtError removeAllListeners();
+  rtError removeAllListenersByName(const rtString& eventName);
 
   void setStatusCode(int32_t v);
   void setErrorMessage(const rtString& v);
