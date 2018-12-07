f85efad8c60098e9e80439cd918e9148af6e2e36
madanagopaltcomcast <35588028+madanagopaltcomcast@users.noreply.github.com>
2018-12-07 10:58:46 -0500

    Update pxScene2d.cpp

--- examples/pxScene2d/src/pxScene2d.cpp
+++ examples/pxScene2d/src/pxScene2d.cpp
@@ -2025,6 +2025,7 @@ pxScene2d::pxScene2d(bool top, pxScriptView* scriptView)
 rtError pxScene2d::dispose()
 {
     mDisposed = true;
+    mMouseEntered = null;
     rtObjectRef e = new rtMapObject;
     // pass false to make onClose asynchronous
     mEmit.send("onClose", e);
@@ -2674,13 +2675,13 @@ void pxScene2d::onUpdate(double t)
   // Periodically let's poke the onMouseMove handler with the current pointer position
   // to better handle objects that animate in or out from under the mouse cursor
   // eg. scrolling
-  /*
+  
   if (t-mPointerLastUpdated > 1) // Once a second
   {
     updateMouseEntered();
     mPointerLastUpdated = t;
   }
-*/
+
   #ifdef ENABLE_RT_NODE
   if (mTop)
   {
