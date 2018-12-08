3cc712be7a8e20b349f428b00fec3030089c3f09
madanagopaltcomcast <35588028+madanagopaltcomcast@users.noreply.github.com>
2018-12-07 21:43:50 -0500

    Update pxScene2d.cpp

--- examples/pxScene2d/src/pxScene2d.cpp
+++ examples/pxScene2d/src/pxScene2d.cpp
@@ -2675,7 +2675,6 @@ void pxScene2d::onUpdate(double t)
   // Periodically let's poke the onMouseMove handler with the current pointer position
   // to better handle objects that animate in or out from under the mouse cursor
   // eg. scrolling
-  
   if (t-mPointerLastUpdated > 1) // Once a second
   {
     updateMouseEntered();
