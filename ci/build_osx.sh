a6ac2b2d13b0ad9cc7e0ed816448fb64c5eebb08
mthirumalai <madanagopal_thirumalai@comcast.com>
2019-01-07 09:20:22 +0530

    find the place of clang_rt

--- ci/build_osx.sh
+++ ci/build_osx.sh
@@ -40,7 +40,8 @@ fi
 cd $TRAVIS_BUILD_DIR;
 mkdir -p temp
 cd  temp
-
+find / -name libclang_rt*
+error
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   echo "***************************** Generating config files ****" >> $BUILDLOGS
