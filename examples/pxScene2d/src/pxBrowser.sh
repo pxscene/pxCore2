4f320e88b849fe43d401dcb307de7cb9a9d1ca2c
Jiangliwu <jiangliwu.ipple@gmail.com>
2019-01-10 21:52:56 +0800

    use source code instead of prebuild for qt

--- examples/pxScene2d/src/pxBrowser.sh
+++ examples/pxScene2d/src/pxBrowser.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-rm -rf plugins
-mkdir plugins
-mkdir plugins/platforms
-cp ../external/qt-5.12.0/prebuild/mac/plugins/platforms/libqcocoa.dylib ./plugins/platforms
-
 export LD_LIBRARY_PATH=./Spark.app/Contents/MacOS/lib.
 export DYLD_LIBRARY_PATH=./Spark.app/Contents/MacOS/lib
 
