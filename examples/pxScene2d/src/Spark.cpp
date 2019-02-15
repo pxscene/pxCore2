68e9e9badb6962c6a79d7ac453914ddc360618f6
AshwiniKalokhe <ashwini_kalokhe@example.com>
2019-02-15 13:57:20 -0500

    cleanup

--- examples/pxScene2d/src/Spark.cpp
+++ examples/pxScene2d/src/Spark.cpp
@@ -719,7 +719,7 @@ if (s && (strcmp(s,"1") == 0))
   rtValue dirtyRectsSetting;
   if (RT_OK == rtSettings::instance()->value("enableDirtyRects", dirtyRectsSetting))
     gDirtyRectsEnabled = dirtyRectsSetting.toString().compare("true") == 0;
-
+    
   // OSX likes to pass us some weird parameter on first launch after internet install
   rtLogInfo("window width = %d height = %d", windowWidth, windowHeight);
   win.init(10, 10, windowWidth, windowHeight, url);
