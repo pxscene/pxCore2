68c84ed80fb090bed1a131f5abe3ff3bb14f7ace
Sergiy Gladkyy <sgladkyy@productengine.com>
2019-03-04 10:59:57 +0200

    XRE2-1791 / 4

--- examples/pxScene2d/src/rcvrcore/Logger.js
+++ examples/pxScene2d/src/rcvrcore/Logger.js
@@ -17,7 +17,7 @@ limitations under the License.
 */
 
 /**
- * Created by tcarro004 on 7/18/15, updated by sgladk001c on 3/02/19.
+ * Created by tcarro004 on 7/18/15, updated by sgladk001c on 3/04/19.
  */
 
 var loggingLevel = 1;
