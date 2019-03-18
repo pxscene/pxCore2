7538226f5d0bbda24a9a6169f22c6008a9433ffd
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2019-03-18 15:27:07 +0200

    remove 2 more places where node 6 hardcoded

--- cmake/V8Deps.cmake
+++ cmake/V8Deps.cmake
@@ -37,7 +37,11 @@ if (PREFER_SYSTEM_LIBRARIES)
 endif (PREFER_SYSTEM_LIBRARIES)
 
 if (NOT V8_FOUND)
-set(NODEDIR "${EXTDIR}/libnode-v6.9.0/")
+if (NOT WIN32)
+    set(NODEDIR "${EXTDIR}/node/")
+else ()
+    set(NODEDIR "${EXTDIR}/libnode-v6.9.0/")
+endif ()
 set(V8DIR "${EXTDIR}/v8/")
 set(V8_INCLUDE_DIRS ${V8DIR}/include
         ${NODEDIR}/deps/uv/include 
