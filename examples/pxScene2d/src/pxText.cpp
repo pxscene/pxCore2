b13e83cac4d2a04a68d5914087f6a93abcdb80e2
Sergiy Gladkyy <sgladkyy@productengine.com>
2019-03-21 12:39:28 +0200

    XRE-1555 upd

--- examples/pxScene2d/src/pxText.cpp
+++ examples/pxScene2d/src/pxText.cpp
@@ -203,9 +203,9 @@ rtError pxText::setFont(rtObjectRef o)
   removeResourceListener();
 
   if (o){
-    rtString desc;
+    rtString descr;
     rtError err = o.sendReturns<rtString>("description", desc);
-    if (err == RT_OK && desc.compare("pxFont") == 0) {
+    if (err == RT_OK && descr.compare("pxFont") == 0) {
         mFont = o;
      }
    }
