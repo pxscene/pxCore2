6df86eadba57cbd721c54e44a9ea2e2dad3ada49
madanagopalt <madanagopal123@gmail.com>
2019-04-01 16:51:03 +0530

    Update pxArchive.cpp

--- examples/pxScene2d/src/pxArchive.cpp
+++ examples/pxScene2d/src/pxArchive.cpp
@@ -68,6 +68,7 @@ void pxArchive::setupArchive()
     mLoadStatus.set("errorString", mErrorString);
 
     if (mDownloadStatusCode == 0) {
+      // this scenario of mismatch is very rare and can happen only if heap object is destroyed before we access
       if ((NULL != mArchiveData) && (mArchiveDataSize > 0))
       {
         mData.init((uint8_t *) mArchiveData, mArchiveDataSize);
