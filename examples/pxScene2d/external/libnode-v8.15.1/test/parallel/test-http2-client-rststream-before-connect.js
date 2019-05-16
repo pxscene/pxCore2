97c1b69e76693e6d11ba881bde596fb111b26a63
Nikita Poltorapavlo <npoltorapavlo@productengine.com>
2019-05-17 00:50:31 +0300

    node 8.15.1 and grpc-js [PoC] (#1735)
    * node 8.11.2
    
    * add @grpc/grpc-js module
    
    * add google-protobuf module
    
    * enable grpc and google-protobuf modules
    
    * grpc demo
    
    * remove Function redefine
    
    * fix 'instanceof Function'
    
    * patch with node-v6.9.0 modifications
    
    * node 8 build steps update
    
    * node 8 inspector
    
    * update build steps for node 8
    
    * mapped http2 to https for grpc demo
    
    * add license header in grpc_demo.js
    
    * node 8 mac support
    
    * update node 8 usage in scripts
    
    * libnode-v8.15.1
    
    * libnode-v8.15.1 modifications
    
    * node build steps update
    
    * remove old patch
    
    * Spark updates for node 8.15.1
    
    * leak suppression for rtScriptNode dtor never called
    
    * build fix for node 6
    
    * fix a unit test that works by chance
    
    * fix places where libnode 6.9.0 hardcoded
    
    * enable node 8.15.1 for Windows
    
    * node build only static for Windows
    
    * remove cctest node target
    
    * remove node sources from pxScene2d cmake
    
    * Revert "node build only static for Windows"
    
    This reverts commit b2ac096b8c4d7060457ad25edaab01495b569599.
    
    * do not build pxScene before pxCore (parallel build issue)
    
    * skip duktape to speed up build
    
    * Revert "skip duktape to speed up build"
    
    This reverts commit 61f5e21c47addb96b127942a23711ff26baf4661.
    
    * Revert "remove cctest node target"
    
    This reverts commit ff86f0af660b7f46b041aaf3c1e520b204019112.
    
    * drop v8_inspector_stl from Windows dependencies
    
    * get rid of old http2
    
    * Revert "drop v8_inspector_stl from Windows dependencies"
    
    This reverts commit 4bc0abb557f7efd0ceb5306f075c57a2d25623df.
    
    * Revert "enable node 8.15.1 for Windows"
    
    This reverts commit 4aace3f41e42af0f1bef99dfcd87b3daa3a361d1.
    
    * remove 2 more places where node 6 hardcoded
    
    * Revert "remove node sources from pxScene2d cmake"
    
    This reverts commit b186683d9e081570e47344467c8af303a18a340f.
    
    * remove redundant files (.ts, sources, semver), remove demo app
    
    * node 8 for Windows
    
    * fix build node 8 for Windows
    
    * Revert "libnode-v8.15.1 modifications"
    
    This reverts commit cccaeaea567cb474399e4342b9092cadc01333cb.
    
    * libnode-v8.15.1 modifications patch
    
    * merge recent changes from node 6 to node 8
    
    * revert code to NOT use ifdef for HAVE_INSPECTOR
    
    * Revert "patch with node-v6.9.0 modifications"
    
    This reverts commit a031c7d31957eb836ab45b4925dc7d908302bfd1.
    
    * --node-version ARG for build.sh, USE_NODE_8 cmake flag
    
    * apply node 8 patch during Winodws build
    
    * add context scope (crash fix)
    
    * add context scope (crash fix, pxscene2dtests)
    
    * add a couple more context scopes
    
    * reduce windows build time and log
    
    * wrap previous change in MODIFIED CODE block
    
    * revert 'static' option for windows build (times out anyway)
    
    * for appveyour cut off node build
    
    * remove windows optimization (build time > 90 min)
    
    * reenable nosnapshot and intl (windows, node 8)
    
    * add --no-optimization flag
    
    * fix var name in vcbuild.bat

---
+++ examples/pxScene2d/external/libnode-v8.15.1/test/parallel/test-http2-client-rststream-before-connect.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const common = require('../common');
+if (!common.hasCrypto)
+  common.skip('missing crypto');
+const assert = require('assert');
+const h2 = require('http2');
+
+const server = h2.createServer();
+server.on('stream', (stream) => {
+  stream.on('close', common.mustCall());
+  stream.respond();
+  stream.end('ok');
+});
+
+server.listen(0, common.mustCall(() => {
+  const client = h2.connect(`http://localhost:${server.address().port}`);
+  const req = client.request();
+  const closeCode = 1;
+
+  common.expectsError(
+    () => req.close(2 ** 32),
+    {
+      type: RangeError,
+      code: 'ERR_OUT_OF_RANGE',
+      message: 'The "code" argument is out of range'
+    }
+  );
+  assert.strictEqual(req.closed, false);
+
+  [true, 1, {}, [], null, 'test'].forEach((notFunction) => {
+    common.expectsError(
+      () => req.close(closeCode, notFunction),
+      {
+        type: TypeError,
+        code: 'ERR_INVALID_CALLBACK',
+        message: 'callback must be a function'
+      }
+    );
+    assert.strictEqual(req.closed, false);
+  });
+
+  req.close(closeCode, common.mustCall());
+  assert.strictEqual(req.closed, true);
+
+  // make sure that destroy is called
+  req._destroy = common.mustCall(req._destroy.bind(req));
+
+  // Second call doesn't do anything.
+  req.close(closeCode + 1);
+
+  req.on('close', common.mustCall((code) => {
+    assert.strictEqual(req.destroyed, true);
+    assert.strictEqual(code, closeCode);
+    server.close();
+    client.close();
+  }));
+
+  req.on('error', common.expectsError({
+    code: 'ERR_HTTP2_STREAM_ERROR',
+    type: Error,
+    message: 'Stream closed with error code NGHTTP2_PROTOCOL_ERROR'
+  }));
+
+  // The `response` event should not fire as the server should receive the
+  // RST_STREAM frame before it ever has a chance to reply.
+  req.on('response', common.mustNotCall());
+
+  // The `end` event should still fire as we close the readable stream by
+  // pushing a `null` chunk.
+  req.on('end', common.mustCall());
+
+  req.resume();
+  req.end();
+}));
