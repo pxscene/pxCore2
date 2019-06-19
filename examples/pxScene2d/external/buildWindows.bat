9a8098fd5db028a1f5d44badef37e575e5fc59c5
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-06-19 10:12:28 -0400

    clean up vc.builds

--- examples/pxScene2d/external/buildWindows.bat
+++ examples/pxScene2d/external/buildWindows.bat
@@ -36,7 +36,7 @@ set buildExternal=0
 set nodeVer="6.9.0"
 if NOT [%APPVEYOR_REPO_COMMIT%] == [] (
     FOR /F "tokens=* USEBACKQ" %%F IN (`git diff --name-only %APPVEYOR_REPO_COMMIT% %APPVEYOR_REPO_COMMIT%~`) DO (
-    echo.%%F|findstr "libgif zlib WinSparkle pthread libpng libjpeg-turbo glew freetype curl jpeg-9a"
+    echo.%%F|findstr "zlib WinSparkle pthread libpng libjpeg-turbo glew freetype curl jpeg-9a"
     if !errorlevel! == 0 (
       set buildExternal=1
       echo. External library files are modified. Need to build external : !buildExternal! .
@@ -124,8 +124,3 @@ cmake ..
 cmake --build . --config Release -- /m
 cd ..
 
-REM ---------- GIF
-REM if EXIST vc.build\builds (
-REM   copy /y vc.build\builds\libgif.lib giflib-5.1.9\libgif.lib
-REM  copy /y vc.build\builds\libgif.lib giflib-5.1.9\libgif.7.lib
-REM )
