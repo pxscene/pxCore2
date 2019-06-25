d46a62b5bc62c4efe1dbea274838a92606a45584
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2019-06-25 17:17:00 +0300

    same fixes for triangle2.js

--- examples/pxScene2d/external/spark-webgl/examples/2-triangle/triangle2.js
+++ examples/pxScene2d/external/spark-webgl/examples/2-triangle/triangle2.js
@@ -105,14 +105,8 @@ function initShaders() {
         }
     }
 
-    gl.useProgram(shaderProgram);
-
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
     shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
-
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 }
@@ -137,7 +131,6 @@ function mvPopMatrix() {
 
 
 function setMatrixUniforms() {
-    gl.useProgram(shaderProgram);
     gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
@@ -287,6 +280,9 @@ function initBuffers() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
     cubeVertexIndexBuffer.itemSize = 1;
     cubeVertexIndexBuffer.numItems = 36;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 }
 
 
@@ -296,6 +292,10 @@ var rCube = 0;
 function drawScene() {
     gl.viewport(0, 0, 1280, 720);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.useProgram(shaderProgram);
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
 
     mat4.perspective(45, 1280 / 720, 0.1, 100.0, pMatrix);
 
@@ -335,6 +335,8 @@ function drawScene() {
 
     mvPopMatrix();
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 }
 
 
