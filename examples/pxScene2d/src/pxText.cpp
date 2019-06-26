0b190775b598f06b5623db70827e1e34445d3958
mfiess200 <michael_fiess@cable.comcast.com>
2019-06-26 17:00:22 -0400

    adding ability to create and expose textures of text and images

--- examples/pxScene2d/src/pxText.cpp
+++ examples/pxScene2d/src/pxText.cpp
@@ -27,7 +27,8 @@
 
 extern pxContext context;
 
-pxText::pxText(pxScene2d* scene):pxObject(scene), mFontLoaded(false), mFontFailed(false), mDirty(true), mFontDownloadRequest(NULL), mListenerAdded(false)
+pxText::pxText(pxScene2d* scene):pxObject(scene), mFontLoaded(false), mFontFailed(false), mDirty(true), mFontDownloadRequest(NULL), mListenerAdded(false),
+                                 mTextFbo()
 
 {
   float c[4] = {1, 1, 1, 1};
@@ -40,6 +41,11 @@ pxText::pxText(pxScene2d* scene):pxObject(scene), mFontLoaded(false), mFontFaile
 pxText::~pxText()
 {
   removeResourceListener();
+  if (mTextFbo.getPtr() != NULL)
+  {
+    mTextFbo->resetFbo();
+  }
+  mTextFbo = NULL;
 }
 
 void pxText::onInit()
@@ -102,6 +108,7 @@ void pxText::resourceReady(rtString readyResolution)
 	
     mDirty=true;  
     mScene->mDirty = true;
+    mTextFbo = NULL;
     // !CLF: ToDo Use pxObject::onTextureReady() and rename it.
     if( mInitialized) 
     {
@@ -223,6 +230,39 @@ rtError pxText::setFont(rtObjectRef o)
   return RT_OK;
 }
 
+rtError pxText::texture(uint32_t &v)
+{
+  v = 0;
+  if (mTextFbo.getPtr() != NULL && mTextFbo->getTexture() != NULL)
+  {
+    v = mTextFbo->getTexture()->getNativeId();
+  }
+  else
+  {
+    pxContextFramebufferRef previousSurface;
+    context.pushState();
+    previousSurface = context.getCurrentFramebuffer();
+    mTextFbo = context.createFramebuffer(getFBOWidth(), getFBOHeight());
+    if (mTextFbo.getPtr())
+    {
+      if (context.setFramebuffer(mTextFbo) == PX_OK)
+      {
+        pxMatrix4f m;
+        context.setMatrix(m);
+        context.setAlpha(1.0);
+        context.clear(getFBOWidth(), getFBOHeight());
+        bool previousDirty = mDirty;
+        draw();
+        mDirty = previousDirty;
+      }
+      context.setFramebuffer(previousSurface);
+      v = mTextFbo->getTexture()->getNativeId();
+    }
+    context.popState();
+  }
+  return RT_OK;
+}
+
 float pxText::getOnscreenWidth()
 {
   // TODO review max texture handling
@@ -234,7 +274,7 @@ float pxText::getOnscreenHeight()
   return (mh > MAX_TEXTURE_HEIGHT?MAX_TEXTURE_HEIGHT:mh);
 }
 
-float pxText::getFBOWidth() 
+float pxText::getFBOWidth()
 { 
   if( mw > MAX_TEXTURE_WIDTH) 
   {
@@ -245,7 +285,7 @@ float pxText::getFBOWidth()
     return mw; 
 }
 
-float pxText::getFBOHeight() 
+float pxText::getFBOHeight()
 { 
   if( mh > MAX_TEXTURE_HEIGHT) 
   {
@@ -309,3 +349,4 @@ rtDefineProperty(pxText, textColor);
 rtDefineProperty(pxText, pixelSize);
 rtDefineProperty(pxText, fontUrl);
 rtDefineProperty(pxText, font);
+rtDefineMethod(pxText, texture);
