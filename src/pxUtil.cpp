8ac20902a5acb4bfc21e8b2b06a29f434f2a1de3
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-07-11 11:37:14 -0400

    Added todo comment

--- src/pxUtil.cpp
+++ src/pxUtil.cpp
@@ -2024,6 +2024,7 @@ rtError pxLoadAGIFImage(const char *imageData, size_t imageDataSize,
                 drawGifImage(obj, x, y, x + w, y + h, rows, map, transparent);
 
                 /* pre-display delay in 0.01sec units */
+				// TODO what if gcb.DelayTime is 0 for animated GIFS
                 s.addBuffer(obj, (double)(gcb.DelayTime / (double)100));
             
                 // Clear the GCB so it doesn't apply to the next frame.
