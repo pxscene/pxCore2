72bc01f4d959c834e24940eb9e7fd6e5f7c1ac9c
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-07-26 14:03:01 -0400

    dump data in destructor max

--- examples/pxScene2d/src/pxResource.cpp
+++ examples/pxScene2d/src/pxResource.cpp
@@ -304,6 +304,10 @@ rtImageResource::~rtImageResource()
   {
     mTexture->setTextureListener(NULL);
   }
+  if (mData.length() != 0)
+  {
+    mData.term();
+  }
 }
 
 unsigned long rtImageResource::Release()
