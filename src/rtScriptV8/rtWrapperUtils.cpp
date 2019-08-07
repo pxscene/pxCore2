d5f81ae4e648c484da24a267278b631b277fa18f
Madana Gopal <madanagopal_thirumalai@comcast.com>
2019-08-07 09:14:38 +0530

    changes for fixing node6,node10 compatibility

--- src/rtScriptV8/rtWrapperUtils.cpp
+++ src/rtScriptV8/rtWrapperUtils.cpp
@@ -498,7 +498,11 @@ rtValue js2rt(v8::Local<v8::Context>& ctx, const Handle<Value>& val, rtWrapperEr
     // from some other native addon. Maybe that would actuall work and fail
     // at a lower level?
     Local<Object> obj = val->ToObject(ctx).ToLocalChecked();
-    if (obj->InternalFieldCount() > 0 && !val->IsArrayBufferView() /*Buffer*/)
+
+    if (obj->InternalFieldCount() > 0
+      && !val->IsArrayBufferView() // JS Buffer
+      && !val->IsPromise() // JS Promise
+      )
     {
       return rtObjectWrapper::unwrapObject(obj);
     }
