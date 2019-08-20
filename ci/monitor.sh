285d524069a5327169f419bc31ab397070c775f3
madanagopaltcomcast <35588028+madanagopaltcomcast@users.noreply.github.com>
2019-08-20 15:34:03 +0530

    Update monitor.sh

--- ci/monitor.sh
+++ ci/monitor.sh
@@ -4,7 +4,4 @@ while [ "$count" -le 3600 ]; do
 	sleep 60;
 	count=$((count+60))
 	echo "travis activities running for $count seconds"
-        if [ "$count" -ge 800 ]; then
-        top -b
-        fi
 done
