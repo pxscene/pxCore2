303cb788f6bfe0e3ed782a9be770153194752f73
Madana Gopal <madanagopal_thirumalai@comcast.com>
2019-09-25 22:13:49 +0530

    new change test

--- ci/after_script.sh
+++ ci/after_script.sh
@@ -24,13 +24,13 @@ then
 fi
 
 cd $TRAVIS_BUILD_DIR
-if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ -z "${TRAVIS_TAG}" ] 
-then
-  tar -cvzf logs.tgz logs/*
-  checkError $? "Unable to compress logs folder" "Check for any previous tasks failed" "Retry"
-  ./ci/deploy_files.sh 96.116.56.119 logs.tgz;
-  checkError $? "Unable to send log files to 96.116.56.119" "Possible reason - Server could be down" "Retry"
-fi
+#if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ -z "${TRAVIS_TAG}" ] 
+#then
+#  tar -cvzf logs.tgz logs/*
+#  checkError $? "Unable to compress logs folder" "Check for any previous tasks failed" "Retry"
+#  ./ci/deploy_files.sh 96.116.56.119 logs.tgz;
+#  checkError $? "Unable to send log files to 96.116.56.119" "Possible reason - Server could be down" "Retry"
+#fi
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ] || [ "$TRAVIS_EVENT_TYPE" = "api" ] || [ ! -z "${TRAVIS_TAG}" ]
 then
--- examples/pxScene2d/external/build_release.sh
+++ examples/pxScene2d/external/build_release.sh
@@ -54,60 +54,23 @@ fi
 
 #--------- OPENSSL
 
-export DISABLE_CCACHE=true
-cd ${OPENSSL_DIR}
-if [ "$(uname)" != "Darwin" ]
-then
-./config -shared  --prefix=`pwd`
-else
-./Configure darwin64-x86_64-cc -shared --prefix=`pwd`
-fi
-make clean
-make "-j1"
-make install -i
-rm -rf libcrypto.a
-rm -rf libssl.a
-rm -rf lib/libcrypto.a
-rm -rf lib/libssl.a
-cd ..
-unset DISABLE_CCACHE
 ln -s ${OPENSSL_DIR} openssl
-export LD_LIBRARY_PATH="${OPENSSL_DIR}/:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="${OPENSSL_DIR}/:$DYLD_LIBRARY_PATH"
 
-#--------- PNG
+#--------- CURL
 
-if [ ! -e ./libpng-1.6.28/.libs/libpng16.16.dylib ] ||
-   [ "$(uname)" != "Darwin" ]
-then
+if [ ! -e $EXT_INSTALL_PATH/lib/libcurl.la ]; then
 
-  banner "PNG"
+  banner "CURL"
 
-  cd png
-  ./configure --prefix=$EXT_INSTALL_PATH
-  make all "-j${make_parallel}"
-  make install
+  cd curl
+
+  if [ "$(uname)" = "Darwin" ]; then
+    #Removing api definition for Yosemite compatibility.
+    sed -i '' '/#define HAVE_CLOCK_GETTIME_MONOTONIC 1/d' lib/curl_config.h
+  fi
   cd ..
 
 fi
-#---------
-
-#--------- JPG
-
-if [ ! -e ./jpg/.libs/libjpeg.9.dylib ] ||
-   [ "$(uname)" != "Darwin" ]
-then
-
-  banner "JPG"
-
-  cd jpg
-  ./configure --prefix=$EXT_INSTALL_PATH
-  make all "-j${make_parallel}"
-  make install
-  cd ..
-
-fi
-#---------
 
 #--------- GIF
 
@@ -133,11 +96,6 @@ fi
 
 fi
 
-if [ ! -e ./.libs/libgif.7.dylib ] ||
-[ "$(uname)" != "Darwin" ]
-then
-    make
-[ -d .libs ] || mkdir -p .libs
 if [ -e libgif.7.dylib ]
 then
     cp libgif.7.dylib .libs/libgif.7.dylib
@@ -149,92 +107,9 @@ then
     cp libgif.so .libs/libgif.so
     cp libutil.so .libs/libutil.so
 fi
-fi
 
 cd ..
 
-#-------- openssl
-
-if [ ! -e $EXT_INSTALL_PATH/lib/libcrypto.$LIBEXTN ]
-then
-  banner "openssl"
-
-  cp -r ${OPENSSL_DIR}/* $EXT_INSTALL_PATH
-fi
-
-#--------
-
-#--------- CURL
-
-if [ ! -e $EXT_INSTALL_PATH/lib/libcurl.la ]; then
-
-  banner "CURL"
-
-  cd curl
-
-  CPPFLAGS="-I${OPENSSL_DIR} -I${OPENSSL_DIR}/include" LDFLAGS="-L${OPENSSL_DIR}/lib -Wl,-rpath,${OPENSSL_DIR}/lib " LIBS="-ldl -lpthread" PKG_CONFIG_PATH=$EXT_INSTALL_PATH/lib/pkgconfig:$PKG_CONFIG_PATH ./configure --with-ssl="${OPENSSL_DIR}" --prefix=$EXT_INSTALL_PATH
-
-  if [ "$(uname)" = "Darwin" ]; then
-    #Removing api definition for Yosemite compatibility.
-    sed -i '' '/#define HAVE_CLOCK_GETTIME_MONOTONIC 1/d' lib/curl_config.h
-  fi
-
-  
-  make all "-j${make_parallel}"
-  make install
-  cd ..
-
-fi
-#---------
-
-#--------- ZLIB
-
-if [ ! -e ./zlib/libz.1.2.11.dylib ] ||
-   [ "$(uname)" != "Darwin" ]
-then
-
-  banner "ZLIB"
-
-  cd zlib
-  ./configure --prefix=$EXT_INSTALL_PATH
-  make all "-j${make_parallel}"
-  make install
-  cd ..
-
-fi
-#---------
-
-#--------- LIBJPEG TURBO (Non -macOS)
-
-if [ "$(uname)" != "Darwin" ]
-then
-
-  banner "TURBO"
-
-  cd libjpeg-turbo
-  git update-index --assume-unchanged Makefile.in
-  git update-index --assume-unchanged aclocal.m4
-  git update-index --assume-unchanged ar-lib
-  git update-index --assume-unchanged compile
-  git update-index --assume-unchanged config.guess
-  git update-index --assume-unchanged config.h.in
-  git update-index --assume-unchanged config.sub
-  git update-index --assume-unchanged configure
-  git update-index --assume-unchanged depcomp
-  git update-index --assume-unchanged install-sh
-  git update-index --assume-unchanged java/Makefile.in
-  git update-index --assume-unchanged ltmain.sh
-  git update-index --assume-unchanged md5/Makefile.in
-  git update-index --assume-unchanged missing
-  git update-index --assume-unchanged simd/Makefile.in
-
-  autoreconf -f -i
-  ./configure --prefix=$EXT_INSTALL_PATH
-  make "-j${make_parallel}"
-  make install
-  cd ..
-
-fi
 #---------
 
 #--------- LIBNODE
@@ -264,57 +139,6 @@ then
   rm node
   ln -sf "libnode-v${NODE_VER}" node
 fi
-export LD_LIBRARY_PATH="`pwd`/node:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="`pwd`/node:$DYLD_LIBRARY_PATH"
-#---------
-
-#--------- uWebSockets
-if [ ! -e ./uWebSockets/libuWS.dylib ] ||
-   [ "$(uname)" != "Darwin" ]
-then
-
-  banner "uWEBSOCKETS"
-
-  cd uWebSockets
-
-  if [ -e Makefile.build ]
-  then
-    mv Makefile.build Makefile
-  fi
-
-  CPPFLAGS="-I${OPENSSL_DIR} -I${OPENSSL_DIR}/include" LDFLAGS="-L${OPENSSL_DIR}/lib -Wl,-rpath,${OPENSSL_DIR}/lib " make
-  cd ..
-
-fi
-#--------
-
-# v8
-# todo - uncomment - for now build v8 with buildV8.sh directly
-#bash buildV8.sh
-
-#-------- BREAKPAD (Non -macOS)
-
-if [ "$(uname)" != "Darwin" ]; then
-  ./breakpad/build.sh
-fi
-#---------
-
-#-------- NANOSVG
-
-  banner "NANOSVG"
-
-./nanosvg/build.sh
-#---------
-
-#-------- DUKTAPE
-
-if [ ! -e dukluv/build/libduktape.a ]
-then
-  banner "DUCKTAPE"
-
-  ./dukluv/build.sh
-fi
-
 #-------- spark-webgl
 cd spark-webgl
 if [ "$(uname)" != "Darwin" ]
@@ -324,16 +148,3 @@ else
   cp $RELEASE_EXTERNALS_PATH/extlibs/node_modules/gles2.node ../../src/node_modules/.
 fi
 cd ..
-
-#-------- 
-
-if [ ! -e sqlite/.libs/libsqlite3.a ]
-then
-  banner "SQLITE"
-
-  cd sqlite
-  autoreconf -f -i
-  ./configure
-  make -j3
-  cd ..
-fi
