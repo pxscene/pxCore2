539cc0451a49b430b9f80f6ed132d75ceee4f306
mfiess200 <michael_fiess@cable.comcast.com>
2019-10-09 16:07:07 -0400

    updates for edge builds

--- examples/pxScene2d/src/macstuff/spark.sh
+++ examples/pxScene2d/src/macstuff/spark.sh
@@ -67,7 +67,7 @@ if [ -e ./version ] && [ -w . ]; then
     ./EngineRunner run -productid org.pxscene.pxscene -version $VERSION -url ${UPDATE_URL} &
   else
     if [[ $updateEdge == "true" ]] || [[ $updateEdge == 1 ]] ; then
-      UPDATE_URL=http://96.116.56.119/edge_webgl/osx/artifacts/software_update.plist
+      UPDATE_URL=http://96.116.56.119/edge/osx/artifacts/software_update.plist
       ./EngineRunner run -productid org.pxscene.pxscene -version $VERSION -url ${UPDATE_URL} &
     fi
   fi
