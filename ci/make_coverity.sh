839c1f9c1c11467a84ad7b7c433e8a5223696f80
Connie Fry <constance_fry@cable.comcast.com>
2019-10-21 19:29:32 -0700

    Changes for externals in different repo

--- ci/make_coverity.sh
+++ ci/make_coverity.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ] ; then exit 1; fi
+export PKG_CONFIG_PATH=$TRAVIS_BUILD_DIR/examples/pxScene2d/external/extlibs/lib/pkgconfig:$PKG_CONFIG_PATH
 cd $TRAVIS_BUILD_DIR
 mkdir temp
 cd temp
-cmake ..
+cmake -DPREFER_SYSTEM_LIBRARIES=ON -DPREFER_PKGCONFIG=ON -DPREFER_EXTERNAL_GIF=ON ..
 cmake --build . --clean-first -- -j1
