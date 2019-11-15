97da2d656e2a39d2c8d979be5176c743f105e4ce
madanagopalt <madanagopal123@gmail.com>
2019-11-15 22:54:51 +0530

    Update build_release.sh

--- examples/pxScene2d/external/build_release.sh
+++ examples/pxScene2d/external/build_release.sh
@@ -94,6 +94,9 @@ cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/node_contextify_mods.h src/.
 cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/node_internals.h src/.
 cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/module_wrap.h src/.
 cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/env-inl.h src/.
+cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/env.h src/.
+cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/stream_wrap.h src/.
+cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/tcp_wrap.h src/.
 cp $RELEASE_EXTERNALS_PATH/extlibs/include/node/node_crypto.h src/.
 cd ..
 rm node
