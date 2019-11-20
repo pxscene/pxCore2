7a2ed186049c9d56b4251400b94407c7f7aa4f74
madanagopalt <madanagopal123@gmail.com>
2019-11-20 22:22:48 +0530

    Update build.sh

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -135,13 +135,13 @@ then
 fi
 
 #setup spark externals repo
-SRC_REPO_USER_NAME='madanagopaltcomcast'
+SRC_REPO_USER_NAME='pxscene'
 DEST_REPO_USER_NAME=`echo $TRAVIS_REPO_SLUG | cut -d'/' -f 1`
 cd $TRAVIS_BUILD_DIR
 cd ../
 mkdir rlExternals
 cd rlExternals
-git clone --branch=jj https://github.com/$SRC_REPO_USER_NAME/Spark-Externals.git
+git clone --branch=master https://github.com/$SRC_REPO_USER_NAME/Spark-Externals.git
 cd Spark-Externals
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   sed -i -n "s/$SRC_REPO_USER_NAME/$DEST_REPO_USER_NAME/g" artifacts/$TRAVIS_OS_NAME/lib/pkgconfig/*
