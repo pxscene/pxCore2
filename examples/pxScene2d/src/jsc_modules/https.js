emutavchi/pxCore

Initial implementation of JSC backend rtScript

examples/pxScene2d/src/jsc_modules/https.js
@@ -1,0 +1,30 @@
+/*
+
+pxCore Copyright 2005-2018 John Robinson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+"use strict";
+
+module.exports = {
+  'get': function (url, cb) {
+    var req = httpGet(url, cb);
+    req.end();
+    return req;
+  },
+  'request': function (options, cb) {
+    return httpGet(options, cb);
+  }
+};

pxscene/pxCore2

examples/pxScene2d/src/jsc_modules/https.js
@@ -1,0 +1,30 @@
+/*
+
+pxCore Copyright 2005-2018 John Robinson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+"use strict";
+
+module.exports = {
+  'get': function (url, cb) {
+    var req = httpGet(url, cb);
+    req.end();
+    return req;
+  },
+  'request': function (url, cb) {
+    return httpGet(url, cb);
+  }
+};