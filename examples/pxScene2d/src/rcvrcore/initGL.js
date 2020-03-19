a88e224f7c555d91984361c1ccaa552cdc6dce10
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2020-03-19 16:12:21 +0200

    add globals: bootstrap, sparkHash

--- examples/pxScene2d/src/rcvrcore/initGL.js
+++ examples/pxScene2d/src/rcvrcore/initGL.js
@@ -350,6 +350,7 @@ function LightningApp(params) {
   tmpGlobal.endDrawing = params._endDrawing
   tmpGlobal.sparkscene = params.sparkscene
   tmpGlobal.sparkHttp = params._sparkHttp
+  tmpGlobal.bootstrap = params.bootstrap;
   // JRJR review this... if we don't draw outside of the timers
   // then no need for this... 
   // general todo... in terms of sandboxing webgl operations.
@@ -371,6 +372,7 @@ function LightningApp(params) {
   tmpGlobal.setImmediate = xxsetImmediate.bind(this)
   tmpGlobal.clearImmediate = xxclearImmediate.bind(this)
   tmpGlobal.sparkQueryParams = urlmain.parse(this.url, true).query;
+  tmpGlobal.sparkHash = urlmain.parse(this.url, true).hash;
   tmpGlobal.thisIsTmpGlobal = true;
 
   var tmpSandbox = this.sandbox
